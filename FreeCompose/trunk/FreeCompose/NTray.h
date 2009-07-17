4ddd694b82c2ae2b933f9e36f7ba2ab54c21e9f1
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-07-17 19:38:13 +0000

    Update to CTrayNotifyIcon v1.61 from
    http://www.naughter.com/ntray.html

--- FreeCompose/trunk/FreeCompose/NTray.h
+++ FreeCompose/trunk/FreeCompose/NTray.h
@@ -2,9 +2,8 @@
 Module : NTray.h
 Purpose: Interface for a MFC class to encapsulate Shell_NotifyIcon
 Created: PJN / 14-05-1997
-History: None
 
-Copyright (c) 1997 - 2006 by PJ Naughter (Web: www.naughter.com, Email: pjna@naughter.com)
+Copyright (c) 1997 - 2008 by PJ Naughter (Web: www.naughter.com, Email: pjna@naughter.com)
 
 All rights reserved.
 
@@ -21,15 +20,13 @@ to maintain a single distribution point for the source code.
 
 /////////////////////////// Macros / Defines ///////////////////////////
 
+#pragma once
+
 #ifndef _NTRAY_H__
 #define _NTRAY_H__
 
 #ifndef CTRAYNOTIFYICON_EXT_CLASS
 #define CTRAYNOTIFYICON_EXT_CLASS
-#endif
-
-#ifndef CTRAYNOTIFYICON_EXT_API
-#define CTRAYNOTIFYICON_EXT_API
 #endif
 
 #ifndef __ATLWIN_H__
@@ -38,7 +35,7 @@ to maintain a single distribution point for the source code.
 
 #ifndef _AFX
 #include <atlmisc.h> //If you do want to use CTrayNotifyIcon independent of MFC, then you need to download and install WTL from http://sourceforge.net/projects/wtl
-#define CTrayNotifyIconString WTL::CString
+#define CTrayNotifyIconString _CSTRING_NS::CString
 #else
 #define CTrayNotifyIconString CString
 #endif
@@ -46,45 +43,8 @@ to maintain a single distribution point for the source code.
 
 /////////////////////////// Classes ///////////////////////////////////////////
 
-//forward declaration
-class CTrayNotifyIcon;
-
-//Class which handles subclassing the top level window and handles the timer
-//required for tray icon animation and taskbar creation message
-class CTRAYNOTIFYICON_EXT_CLASS CTrayIconHooker : public CWindowImpl<CTrayIconHooker>
-{
-public:
-//Constructors / Destructors
-  CTrayIconHooker();
-  ~CTrayIconHooker();
-
-//Methods
-#ifdef _AFX
-  BOOL Init(CTrayNotifyIcon* pTrayIcon, CWnd* pNotifyWnd);
-#else
-  BOOL Init(CTrayNotifyIcon* pTrayIcon, CWindow* pNotifyWnd);
-#endif  
-  void StartUsingAnimation(HICON* phIcons, int nNumIcons, DWORD dwDelay);
-  void StopUsingAnimation();
-  BOOL UsingAnimatedIcon() const;
-  HICON GetCurrentIcon() const;
-
-protected:
-//Methods
-  virtual BOOL ProcessWindowMessage(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam, LRESULT& lResult, DWORD dwMsgMapID);
-  void OnTimer(UINT_PTR nIDEvent);
-  LRESULT OnTaskbarCreated(WPARAM wParam, LPARAM lParam);
-
-//Member variables
-  CTrayNotifyIcon* m_pTrayIcon;
-  HICON*           m_phIcons;
-  int              m_nNumIcons;
-  UINT_PTR         m_nTimerID;
-  int              m_nCurrentIconIndex;
-};
-
 //the actual tray notification class wrapper
-class CTRAYNOTIFYICON_EXT_CLASS CTrayNotifyIcon
+class CTRAYNOTIFYICON_EXT_CLASS CTrayNotifyIcon : public CWindowImpl<CTrayNotifyIcon>
 {
 public:
 //Enums / Typedefs
@@ -156,25 +116,51 @@ public:
     GUID guidItem;
   } NOTIFYICONDATA_3;
 
+  typedef struct _NOTIFYICONDATA_4 //The version of the structure supported by Shell v7
+  {
+    DWORD cbSize;
+    HWND hWnd;
+    UINT uID;
+    UINT uFlags;
+    UINT uCallbackMessage;
+    HICON hIcon;
+    TCHAR szTip[128];
+    DWORD dwState;
+    DWORD dwStateMask;
+    TCHAR szInfo[256];
+    union 
+    {
+      UINT uTimeout;
+      UINT uVersion;
+    } DUMMYUNIONNAME;
+    TCHAR szInfoTitle[64];
+    DWORD dwInfoFlags;
+    GUID guidItem;
+    HICON hBalloonIcon;
+  } NOTIFYICONDATA_4;
+
+  DECLARE_WND_CLASS(_T("TrayNotifyIconClass"))
+
+
 //Constructors / Destructors
   CTrayNotifyIcon();
   virtual ~CTrayNotifyIcon();
 
 //Create the tray icon
 #ifdef _AFX
-  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, HICON hIcon, UINT nNotifyMessage, UINT uMenuID = 0);
-  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, CBitmap* pBitmap, UINT nNotifyMessage, UINT uMenuID = 0);
-  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, HICON* phIcons, int nNumIcons, DWORD dwDelay, UINT nNotifyMessage, UINT uMenuID = 0);
-  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, HICON hIcon, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE);                  
-  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, CBitmap* pBitmap, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE);
-  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, HICON* phIcons, int nNumIcons, DWORD dwDelay, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE);
+  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, HICON hIcon, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bShow = TRUE);
+  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, CBitmap* pBitmap, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bShow = TRUE);
+  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, HICON* phIcons, int nNumIcons, DWORD dwDelay, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bShow = TRUE);
+  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, HICON hIcon, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE, BOOL bLargeIcon = FALSE, BOOL bRealtime = FALSE, HICON hBalloonIcon = NULL, BOOL bShow = TRUE);                  
+  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, CBitmap* pBitmap, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE, BOOL bLargeIcon = FALSE, BOOL bRealtime = FALSE, HICON hBalloonIcon = NULL, BOOL bShow = TRUE);
+  BOOL Create(CWnd* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, HICON* phIcons, int nNumIcons, DWORD dwDelay, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE, BOOL bLargeIcon = FALSE, BOOL bRealtime = FALSE, HICON hBalloonIcon = NULL, BOOL bShow = TRUE);
 #else
-  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, HICON hIcon, UINT nNotifyMessage, UINT uMenuID = 0);
-  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, CBitmap* pBitmap, UINT nNotifyMessage, UINT uMenuID = 0);
-  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, HICON* phIcons, int nNumIcons, DWORD dwDelay, UINT nNotifyMessage, UINT uMenuID = 0);
-  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, HICON hIcon, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE);                  
-  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, CBitmap* pBitmap, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE);
-  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, HICON* phIcons, int nNumIcons, DWORD dwDelay, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE);
+  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, HICON hIcon, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bShow = TRUE);
+  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, CBitmap* pBitmap, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bShow = TRUE);
+  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, HICON* phIcons, int nNumIcons, DWORD dwDelay, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bShow = TRUE);
+  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, HICON hIcon, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE, BOOL bLargeIcon = FALSE, BOOL bRealtime = FALSE, HICON hBalloonIcon = NULL, BOOL bShow = TRUE);                  
+  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, CBitmap* pBitmap, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE, BOOL bLargeIcon = FALSE, BOOL bRealtime = FALSE, HICON hBalloonIcon = NULL, BOOL bShow = TRUE);
+  BOOL Create(CWindow* pNotifyWnd, UINT uID, LPCTSTR pszTooltipText, LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, UINT nTimeout, BalloonStyle style, HICON* phIcons, int nNumIcons, DWORD dwDelay, UINT nNotifyMessage, UINT uMenuID = 0, BOOL bNoSound = FALSE, BOOL bLargeIcon = FALSE, BOOL bRealtime = FALSE, HICON hBalloonIcon = NULL, BOOL bShow = TRUE);
 #endif  
 
 //Sets or gets the Tooltip text
@@ -203,9 +189,10 @@ public:
 #endif
 
 //Modification of the tray icons
-	void HideIcon();
-	void ShowIcon();
-	void RemoveIcon();
+  BOOL Delete(BOOL bCloseHelperWindow = TRUE);
+  BOOL Create(BOOL bShow = TRUE);
+  BOOL Hide();
+  BOOL Show();
 
 //Dynamic tray icon support
   HICON BitmapToIcon(CBitmap* pBitmap);
@@ -225,7 +212,7 @@ public:
   BOOL IsHidden() const { return m_bHidden; };
 
 //Sets or gets the Balloon style tooltip settings
-  BOOL                  SetBalloonDetails(LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, BalloonStyle style, UINT nTimeout, HICON hUserIcon=NULL, BOOL bNoSound=FALSE);
+  BOOL                  SetBalloonDetails(LPCTSTR pszBalloonText, LPCTSTR pszBalloonCaption, BalloonStyle style, UINT nTimeout, HICON hUserIcon = NULL, BOOL bNoSound = FALSE, BOOL bLargeIcon = FALSE, BOOL bRealtime = FALSE, HICON hBalloonIcon = NULL);
   CTrayNotifyIconString GetBalloonText() const;
   CTrayNotifyIconString GetBalloonCaption() const;
   BalloonStyle          GetBalloonStyle() const;
@@ -234,15 +221,36 @@ public:
 //Other functionality
   BOOL SetVersion(UINT uVersion);
   BOOL SetFocus();
-  static DWORD GetShellVersion();
 
 //Helper functions to load tray icon from resources
-  static HICON LoadIconResource(LPCTSTR lpIconName);
-  static HICON LoadIconResource(UINT nIDResource);
+  static HICON LoadIcon(LPCTSTR lpIconName, BOOL bLargeIcon = FALSE);
+  static HICON LoadIcon(UINT nIDResource, BOOL bLargeIcon = FALSE);
+  static HICON LoadIcon(HINSTANCE hInstance, LPCTSTR lpIconName, BOOL bLargeIcon = FALSE);
+  static HICON LoadIcon(HINSTANCE hInstance, UINT nIDResource, BOOL bLargeIcon = FALSE);
 
 protected:
+//Methods
+  BOOL         CreateHelperWindow();
+  void         StartAnimation(HICON* phIcons, int nNumIcons, DWORD dwDelay);
+  void         StopAnimation();
+  HICON        GetCurrentAnimationIcon() const;
+  virtual BOOL ProcessWindowMessage(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam, LRESULT& lResult, DWORD dwMsgMapID);
+  LRESULT      OnTaskbarCreated(WPARAM wParam, LPARAM lParam);
+  void         OnTimer(UINT_PTR nIDEvent);
+  void         OnDestroy();
+  DWORD        GetNOTIFYICONDATASizeForOS();
+
+//Enums
+  enum ShellVersion
+  {
+    Version4     = 0, //PreWin2k
+    Version5     = 1, //Win2k
+    Version6     = 2, //XP
+    VersionVista = 3  //Vista
+  };
+
 //Member variables
-  NOTIFYICONDATA_3 m_NotifyIconData;
+  NOTIFYICONDATA_4 m_NotifyIconData;
   BOOL             m_bCreated;
   BOOL             m_bHidden;
 #ifdef _AFX  
@@ -250,16 +258,15 @@ protected:
 #else
   CWindow*         m_pNotificationWnd;
 #endif  
-  CTrayIconHooker  m_HookWnd;
   CMenu            m_Menu;
   UINT             m_nDefaultMenuItem;
   BOOL             m_bDefaultMenuItemByPos;
-  static DWORD     sm_dwShellVersion;
-
-//Methods
-  static DWORD GetNOTIFYICONDATASizeForOS();
-
-  friend class CTrayIconHooker;
+  ShellVersion     m_ShellVersion;
+  HICON            m_hDynamicIcon; //Our cached copy of the last icon created with BitmapToIcon
+  HICON*           m_phIcons;
+  int              m_nNumIcons;
+  UINT_PTR         m_nTimerID;
+  int              m_nCurrentIconIndex;
 };
 
 #endif //_NTRAY_H__
