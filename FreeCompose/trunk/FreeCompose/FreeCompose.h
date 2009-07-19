075c34912e40fd2b1092c77af03aba1b39596ca0
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-07-19 19:59:53 +0000

    Add another helper for sorting backwards.

--- FreeCompose/trunk/FreeCompose/FreeCompose.h
+++ FreeCompose/trunk/FreeCompose/FreeCompose.h
@@ -52,3 +52,14 @@ template<typename T> inline T compare_keys( void* /*context*/, const void* _elem
 		return 0;
 }
 
+template<typename T> inline T compare_keys_reverse( void* /*context*/, const void* _elem2, const void* _elem1 ) {
+	T elem1 = *( (T*) _elem1 );
+	T elem2 = *( (T*) _elem2 );
+
+	if ( elem1 < elem2 )
+		return -1;
+	else if ( elem1 > elem2 )
+		return 1;
+	else
+		return 0;
+}
