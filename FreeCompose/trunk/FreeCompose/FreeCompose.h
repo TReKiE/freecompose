662b65d56b5e6c2e1eef4a22a1ef972a0433eb84
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-07-19 19:03:10 +0000

    Add helper function that was too good to throw away when I no
    longer needed it.

--- FreeCompose/trunk/FreeCompose/FreeCompose.h
+++ FreeCompose/trunk/FreeCompose/FreeCompose.h
@@ -39,3 +39,16 @@ extern CFreeComposeApp theApp;
 extern CArray< COMPOSE_KEY_ENTRY > ComposeKeyEntries;
 
 void debug( LPCTSTR lpsz, ... );
+
+template<typename T> inline T compare_keys( void* /*context*/, const void* _elem1, const void* _elem2 ) {
+	T elem1 = *( (T*) _elem1 );
+	T elem2 = *( (T*) _elem2 );
+
+	if ( elem1 < elem2 )
+		return -1;
+	else if ( elem1 > elem2 )
+		return 1;
+	else
+		return 0;
+}
+
