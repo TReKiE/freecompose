aa2fe939e9783149440925943cb440ff156f8cee
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-07-19 18:32:11 +0000

    Add debug() function.

--- FreeCompose/trunk/FreeCompose/FreeCompose.cpp
+++ FreeCompose/trunk/FreeCompose/FreeCompose.cpp
@@ -138,3 +138,13 @@ void CFreeComposeApp::OnAppConfigure()
 //{
 //	return CWinApp::ExitInstance();
 //}
+
+void debug( LPCTSTR lpsz, ... ) {
+	TCHAR szbuf[1024];
+	va_list va;
+
+	va_start( va, lpsz );
+	_vsntprintf_s( szbuf, 1024, _TRUNCATE, lpsz, va );
+	va_end( va );
+	OutputDebugString( szbuf );
+}
