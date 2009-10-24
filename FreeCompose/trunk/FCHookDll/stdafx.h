8f45e200a232f8f8ba6b01e3ed24676cd975b2b5
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-10-24 23:02:05 +0000

    Update Windows version build requirements.

--- FreeCompose/trunk/FCHookDll/stdafx.h
+++ FreeCompose/trunk/FCHookDll/stdafx.h
@@ -6,21 +6,24 @@
 
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
-#ifndef WINVER				// Allow use of features specific to Windows XP or later.
-#define WINVER 0x0501		// Change this to the appropriate value to target other versions of Windows.
-#endif
 
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
-#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
-#endif						
+#undef NTDDI_VERSION
+#undef WINVER
+#undef _WIN32_IE
+#undef _WIN32_WINDOWS
+#undef _WIN32_WINNT
 
-#ifndef _WIN32_WINDOWS		// Allow use of features specific to Windows 98 or later.
-#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
-#endif
+// Allow use of features specific to Windows XP or later.
+#define NTDDI_VERSION NTDDI_WINXP
+#define WINVER 0x0501
+#define _WIN32_WINNT 0x0501
 
-#ifndef _WIN32_IE			// Allow use of features specific to IE 6.0 or later.
-#define _WIN32_IE 0x0600	// Change this to the appropriate value to target other versions of IE.
-#endif
+// _WIN32_WINDOWS and _WIN32_IE are set low on purpose
+// MFC requires _WIN32_WINDOWS >= 0x0400
+// Common Controls on XP requires _WIN32_IE >= 0x0500
+#define _WIN32_WINDOWS 0x0400
+#define _WIN32_IE 0x0500
+
 
 #include <windows.h>
 
