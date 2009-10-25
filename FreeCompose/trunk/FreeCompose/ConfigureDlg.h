d46a88905aebfdd76e9f4bbc19c25ae14fe64292
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-10-25 17:07:23 +0000

    Big rework to use a CPropertySheet instead of a plain CDialog.

--- FreeCompose/trunk/FreeCompose/ConfigureDlg.h
+++ FreeCompose/trunk/FreeCompose/ConfigureDlg.h
@@ -2,18 +2,18 @@
 #include "afxcmn.h"
 
 
-// CConfigureDlg dialog
+// COptionsDlg dialog
 
-class CConfigureDlg : public CDialog
+class COptionsDlg : public CDialog
 {
-	DECLARE_DYNAMIC(CConfigureDlg)
+	DECLARE_DYNAMIC(COptionsDlg)
 
 public:
-	CConfigureDlg(CWnd* pParent = NULL);   // standard constructor
-	virtual ~CConfigureDlg();
+	COptionsDlg(CWnd* pParent = NULL);   // standard constructor
+	virtual ~COptionsDlg();
 
 // Dialog Data
-	enum { IDD = IDD_CONFIGURE };
+	enum { IDD = IDD_OPTIONS };
 
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
@@ -21,19 +21,28 @@ protected:
 	DECLARE_MESSAGE_MAP()
 
 private:
-	CListCtrl m_KeyComboList;
-	CSize m_MinimumSize;
-	CButton m_btnEdit;
-	CButton m_btnRemove;
-	CButton m_btnClose;
+	//CSize m_MinimumSize;
 	int m_nColumnWidths[3];
 
-	void _DoLayout( const int excessX = 0, const int excessY = 0 );
+	CTabCtrl m_Tabs;
+
+	CListCtrl m_KeyComboList;
+	CButton m_btnEdit;
+	CButton m_btnRemove;
+
+	CButton m_btnOk;
+	CButton m_btnApply;
+	CButton m_btnClose;
+
+	//void _DoLayout( const int excessX = 0, const int excessY = 0 );
 	void _FillKeyComboList( void );
 
 	virtual BOOL OnInitDialog();
-	afx_msg void OnSizing(UINT fwSide, LPRECT pRect);
+	//afx_msg void OnSizing(UINT fwSide, LPRECT pRect);
 	afx_msg void OnKeyComboListItemChanged(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnBnClickedEdit();
 	afx_msg void OnBnClickedRemove();
-	afx_msg void OnBnClickedClose();
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedApply();
+	afx_msg void OnBnClickedCancel();
 };
