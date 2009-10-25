91c6da02209badb457d8c289380f84112faa8c09
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-10-25 17:07:57 +0000

    Custom ByteSet class, for holding integers 0..255.

--- FreeCompose/trunk/FCHookDll/Common.h
+++ FreeCompose/trunk/FCHookDll/Common.h
@@ -15,3 +15,22 @@ __declspec(selectany) volatile bool      fDisableCapsLock   = false;
 
 void debug( LPCTSTR lpszMessage, ... );
 int CompareCkes( const void*, const void* );
+
+
+class ByteSet {
+public:
+	ByteSet( ) {
+		Clear( );
+		count = 0;
+	}
+
+	inline void Clear   ( void )                { for ( int n = 0; n < countof( bits ); n++ ) bits[n] = 0;               }
+	inline void Add     ( const DWORD x )       { if ( ! Contains( x ) ) count++; bits[ x >> 3 ] |=  ( 1 << ( x & 7 ) ); }
+	inline void Remove  ( const DWORD x )       { if (   Contains( x ) ) count--; bits[ x >> 3 ] &= ~( 1 << ( x & 7 ) ); }
+	inline bool Contains( const DWORD x ) const { return ( bits[ x >> 3 ] & ( 1 << ( x & 7 ) ) ) != 0;                   }
+	inline bool IsEmpty ( void )          const { return count == 0;                                                     }
+
+private:
+	unsigned char bits[32];
+	int count;
+};
