2e99626cf9a6ea8357ef91f0e4679930ecae9b2f
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-10-25 00:07:24 +0000

    Move debug() and compare_keys*() to Utils.{cpp,h}.

--- FreeCompose/trunk/FreeCompose/FreeCompose.cpp
+++ FreeCompose/trunk/FreeCompose/FreeCompose.cpp
@@ -14,16 +14,6 @@
 #endif
 
 
-void debug( LPCTSTR lpsz, ... ) {
-	TCHAR szbuf[1024];
-	va_list va;
-
-	va_start( va, lpsz );
-	_vsntprintf_s( szbuf, 1024, _TRUNCATE, lpsz, va );
-	va_end( va );
-	OutputDebugString( szbuf );
-}
-
 // CFreeComposeApp
 
 
