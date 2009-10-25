da9ecccbc0190b3c80b397c3b6c04b8fc4afd1e3
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-10-25 00:10:39 +0000

    Add CompareCkes()

--- FreeCompose/trunk/FCHookDll/Common.cpp
+++ FreeCompose/trunk/FCHookDll/Common.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "FCHookDll.h"
+#include "HookProc.h"
 #include "Common.h"
 
 #ifdef _DEBUG
@@ -14,4 +15,23 @@ void debug( LPCTSTR lpszMessage, ... ) {
 	_vsntprintf_s( buf, countof( buf ), _TRUNCATE, lpszMessage, va );
 	va_end( va );
 	OutputDebugString( buf );
+}
+
+int CompareCkes( const void* pv1, const void* pv2 ) {
+	COMPOSE_KEY_ENTRY* pcke1 = (COMPOSE_KEY_ENTRY*) pv1;
+	COMPOSE_KEY_ENTRY* pcke2 = (COMPOSE_KEY_ENTRY*) pv2;
+
+	if ( pcke1->vkFirst < pcke2->vkFirst ) {
+		return -1;
+	} else if ( pcke1->vkFirst == pcke2->vkFirst ) {
+		if ( pcke1->vkSecond < pcke2->vkSecond ) {
+			return -1;
+		} else if ( pcke1->vkSecond == pcke2->vkSecond ) {
+			return 0;
+		} else {
+			return 1;
+		}
+	} else {
+		return 1;
+	}
 }
--- FreeCompose/trunk/FCHookDll/Common.h
+++ FreeCompose/trunk/FCHookDll/Common.h
@@ -2,11 +2,16 @@
 
 #define countof(x) ( sizeof((x)) / sizeof((x)[0]) )
 
+#define LOCK(x) EnterCriticalSection(&(x));
+#define UNLOCK(x) LeaveCriticalSection(&(x));
+
 #pragma data_seg( push, ".shareddata" )
-__declspec(selectany) CRITICAL_SECTION cs;
-__declspec(selectany) COMPOSE_KEY_ENTRY* ComposeKeyEntries = NULL;
-__declspec(selectany) int cComposeKeyEntries = 0;
-__declspec(selectany) HHOOK hHook = NULL;
+__declspec(selectany) CRITICAL_SECTION   cs;
+__declspec(selectany) COMPOSE_KEY_ENTRY* ComposeKeyEntries  = NULL;
+__declspec(selectany) int                cComposeKeyEntries = 0;
+__declspec(selectany) HHOOK              hHook              = NULL;
+__declspec(selectany) volatile bool      fDisableCapsLock   = false;
 #pragma data_seg( pop )
 
 void debug( LPCTSTR lpszMessage, ... );
+int CompareCkes( const void*, const void* );
