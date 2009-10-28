09c858a091b46a65ee4f5eee1aa90d5315c24790
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-10-28 04:43:15 +0000

    Cleanups; use a count instead of a terminator element; handle app commands here instead of CFreeCompose; use ON_UPDATE_COMMAND_UI instead of menu-mashing for dynamically
    updating items.

--- FreeCompose/trunk/FreeCompose/MainFrm.h
+++ FreeCompose/trunk/FreeCompose/MainFrm.h
@@ -5,10 +5,6 @@
 #include "ChildView.h"
 #include "NTray.h"
 
-extern const UINT FCM_ENABLE;
-extern const UINT FCM_DISABLE;
-extern const UINT FCM_CAPSLOCK;
-
 class CMainFrame: public CFrameWnd {
 public:
 	CMainFrame( );
@@ -28,26 +24,26 @@ public:
 
 // Implementation
 protected:
-	HICON m_hIcon;
 	CTrayNotifyIcon* m_ptni;
 	BOOL m_fActive;
-	CMenu m_popup;
 
 // Generated message map functions
 protected:
 	DECLARE_MESSAGE_MAP( )
 
 	afx_msg int OnCreate( LPCREATESTRUCT lpCreateStruct );
-	afx_msg LRESULT OnFcmNotifyIcon( WPARAM, LPARAM );
-	afx_msg LRESULT OnFcmEnable( WPARAM, LPARAM );
-	afx_msg LRESULT OnFcmDisable( WPARAM, LPARAM );
-	afx_msg LRESULT OnFcmCapsLock( WPARAM, LPARAM );
+	afx_msg LRESULT OnNotifyIcon( WPARAM, LPARAM );
 	afx_msg LRESULT OnFcmPip( WPARAM wPip, LPARAM );
-	afx_msg LRESULT OnFcmKey( WPARAM wKey, LPARAM );
-	afx_msg LRESULT OnTaskbarCreated( WPARAM, LPARAM );
 	afx_msg void OnClose( );
+	afx_msg void OnAppAbout( );
+	afx_msg void OnAppEnable( );
+	afx_msg void OnAppDisable( );
+	afx_msg void OnAppCapsLock( );
+	afx_msg void OnAppConfigure( );
+	afx_msg void OnUpdateAppEnable( CCmdUI* );
+	afx_msg void OnUpdateAppCapsLock( CCmdUI* );
 
 private:
 	void _InitializeHook( void );
-	bool _IsCapsLock( void );
+	void _SetupTrayIcon( void );
 };
