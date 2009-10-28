037631cdac6c5c88e7a4ada45b297828465cb135
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-10-28 20:50:32 +0000

    Make it more obvious what compare_keys_reverse() does.

--- FreeCompose/trunk/FreeCompose/Utils.h
+++ FreeCompose/trunk/FreeCompose/Utils.h
@@ -15,13 +15,13 @@ template<typename T> inline T compare_keys( void* /*context*/, const void* _elem
 		return 0;
 }
 
-template<typename T> inline T compare_keys_reverse( void* /*context*/, const void* _elem2, const void* _elem1 ) {
+template<typename T> inline T compare_keys_reverse( void* /*context*/, const void* _elem1, const void* _elem2 ) {
 	T elem1 = *( (T*) _elem1 );
 	T elem2 = *( (T*) _elem2 );
 
-	if ( elem1 < elem2 )
+	if ( elem1 > elem2 )
 		return -1;
-	else if ( elem1 > elem2 )
+	else if ( elem1 < elem2 )
 		return 1;
 	else
 		return 0;
