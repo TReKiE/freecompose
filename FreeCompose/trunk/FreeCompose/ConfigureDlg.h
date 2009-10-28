ca883c25334b5e32191f0c20c4600b32861a8374
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-10-28 05:00:13 +0000

    Rip out key sequence stuff into new property page class.

--- FreeCompose/trunk/FreeCompose/ConfigureDlg.h
+++ FreeCompose/trunk/FreeCompose/ConfigureDlg.h
@@ -1,46 +1,28 @@
 #pragma once
 
-// COptionsDlg dialog
-
-class COptionsDlg : public CDialog
-{
-	DECLARE_DYNAMIC(COptionsDlg)
+class COptionsDlg: public CDialog {
+	DECLARE_DYNAMIC( COptionsDlg )
 
 public:
-	COptionsDlg(CWnd* pParent = NULL);   // standard constructor
-	virtual ~COptionsDlg();
+	COptionsDlg( CWnd* pParent = NULL );
+	virtual ~COptionsDlg( );
 
-// Dialog Data
 	enum { IDD = IDD_OPTIONS };
 
 protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	virtual void DoDataExchange( CDataExchange* pDX );
 
-	DECLARE_MESSAGE_MAP()
+	DECLARE_MESSAGE_MAP( )
 
 private:
-	//CSize m_MinimumSize;
-	int m_nColumnWidths[3];
-
 	CTabCtrl m_Tabs;
-
-	CListCtrl m_KeyComboList;
-	CButton m_btnEdit;
-	CButton m_btnRemove;
 
 	CButton m_btnOk;
 	CButton m_btnApply;
 	CButton m_btnClose;
 
-	//void _DoLayout( const int excessX = 0, const int excessY = 0 );
-	void _FillKeyComboList( void );
-
-	virtual BOOL OnInitDialog();
-	//afx_msg void OnSizing(UINT fwSide, LPRECT pRect);
-	afx_msg void OnKeyComboListItemChanged(NMHDR* pNMHDR, LRESULT* pResult);
-	afx_msg void OnBnClickedEdit();
-	afx_msg void OnBnClickedRemove();
-	afx_msg void OnBnClickedOk();
-	afx_msg void OnBnClickedApply();
-	afx_msg void OnBnClickedCancel();
+	virtual BOOL OnInitDialog( );
+	afx_msg void OnBnClickedOk( );
+	afx_msg void OnBnClickedApply( );
+	afx_msg void OnBnClickedCancel( );
 };
