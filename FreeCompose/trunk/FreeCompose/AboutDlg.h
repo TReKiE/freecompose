f8a260fdfa01dc1fce4846e226b87038f6b67752
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-10-28 04:40:12 +0000

    Cleanies.

--- FreeCompose/trunk/FreeCompose/AboutDlg.h
+++ FreeCompose/trunk/FreeCompose/AboutDlg.h
@@ -1,26 +1,22 @@
 // FreeComposeDlg.h : header file
-//
 
 #pragma once
-#include "afxwin.h"
 
-class CAboutDlg : public CDialog
-{
+class CAboutDlg: public CDialog {
 public:
-	CAboutDlg();
+	CAboutDlg( );
 
 // Dialog Data
 	enum { IDD = IDD_ABOUTBOX };
 
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-	virtual BOOL OnInitDialog();
+protected:
+	virtual void DoDataExchange( CDataExchange* pDX );
+	virtual BOOL OnInitDialog( );
 
 // Implementation
 protected:
-	DECLARE_MESSAGE_MAP()
-public:
 	CString m_strVersion;
 	CString m_strCopyright;
-};
 
+	DECLARE_MESSAGE_MAP( )
+};
--- FreeCompose/trunk/FreeCompose/ConfigureDlg.h
+++ FreeCompose/trunk/FreeCompose/ConfigureDlg.h
@@ -1,6 +1,4 @@
 #pragma once
-#include "afxcmn.h"
-
 
 // COptionsDlg dialog
 
