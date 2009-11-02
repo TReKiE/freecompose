470a236ea56f695858a198edf56653fdfe506ae5
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-11-02 01:50:21 +0000

    Implement Add, Edit

--- FreeCompose/trunk/FreeCompose/KeySequences.h
+++ FreeCompose/trunk/FreeCompose/KeySequences.h
@@ -23,14 +23,19 @@ private:
 	int m_nColumnWidths[3];
 
 	CListCtrl m_KeyComboList;
+	CButton m_btnAdd;
 	CButton m_btnEdit;
 	CButton m_btnRemove;
 
+	void _DoAddOneKeySequence( const int n, const COMPOSE_KEY_ENTRY& cke );
+	void _AdjustColumns( void );
 	void _FillKeyComboList( void );
+	void _AddNewKeySequence( const COMPOSE_KEY_ENTRY& cke );
 
 	virtual BOOL OnInitDialog( );
 
 	afx_msg void OnKeyComboListItemChanged( NMHDR* pNMHDR, LRESULT* pResult );
+	afx_msg void OnBnClickedAdd( );
 	afx_msg void OnBnClickedEdit( );
 	afx_msg void OnBnClickedRemove( );
 };
