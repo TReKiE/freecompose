a7fcd36f5deb5e98654dec03190de5b6143ce7f4
jsc <jsc@bea30d8e-86d1-9815-9a41-0b703c73c987>
2009-11-02 01:50:21 +0000

    Implement Add, Edit

--- FreeCompose/trunk/FreeCompose/KeySequences.cpp
+++ FreeCompose/trunk/FreeCompose/KeySequences.cpp
@@ -4,6 +4,7 @@
 
 #include "FreeCompose.h"
 #include "KeySequences.h"
+#include "EditKeySequence.h"
 
 #include "Utils.h"
 
@@ -14,6 +15,7 @@ IMPLEMENT_DYNAMIC( CKeySequences, CPropertyPage )
 
 BEGIN_MESSAGE_MAP( CKeySequences, CPropertyPage )
 	ON_NOTIFY(LVN_ITEMCHANGED, IDC_KEYCOMBOLIST, &CKeySequences::OnKeyComboListItemChanged)
+	ON_BN_CLICKED(IDADD, &CKeySequences::OnBnClickedAdd)
 	ON_BN_CLICKED(IDEDIT, &CKeySequences::OnBnClickedEdit)
 	ON_BN_CLICKED(IDREMOVE, &CKeySequences::OnBnClickedRemove)
 END_MESSAGE_MAP( )
@@ -31,46 +33,60 @@ CKeySequences::~CKeySequences( ) {
 void CKeySequences::DoDataExchange( CDataExchange* pDX ) {
 	CPropertyPage::DoDataExchange( pDX );
 	DDX_Control( pDX, IDC_KEYCOMBOLIST, m_KeyComboList );
-	DDX_Control( pDX, IDEDIT,           m_btnEdit );
-	DDX_Control( pDX, IDREMOVE,         m_btnRemove );
+	DDX_Control( pDX, IDADD,            m_btnAdd       );
+	DDX_Control( pDX, IDEDIT,           m_btnEdit      );
+	DDX_Control( pDX, IDREMOVE,         m_btnRemove    );
 }
 
-void CKeySequences::_FillKeyComboList( void ) {
+void CKeySequences::_DoAddOneKeySequence( const int n, const COMPOSE_KEY_ENTRY& cke ) {
 	CString col0, col1, col2;
 	int width;
 	BOOL ret;
 
-	m_Options.m_ComposeKeyEntries.RemoveAll( );
-	m_Options.m_ComposeKeyEntries.Copy( ComposeKeyEntries );
+	col0 = cke.wchComposed;
+	width = m_KeyComboList.GetStringWidth( col0 ) + ITEM_FUDGE_FACTOR;
+	if ( width > m_nColumnWidths[0] )
+		m_nColumnWidths[0] = width;
+	int nItem = m_KeyComboList.InsertItem( LVIF_PARAM | LVIF_STATE | LVIF_TEXT, n, col0, 0, (UINT) -1, -1, n );
+	ASSERT( -1 != nItem );
 
-	for ( int n = 0; n < m_Options.m_ComposeKeyEntries.GetCount( ); n++ ) {
-		col0 = m_Options.m_ComposeKeyEntries[n].wchComposed;
-		width = m_KeyComboList.GetStringWidth( col0 ) + ITEM_FUDGE_FACTOR;
-		if ( width > m_nColumnWidths[0] )
-			m_nColumnWidths[0] = width;
-		int nItem = m_KeyComboList.InsertItem( LVIF_PARAM | LVIF_STATE | LVIF_TEXT, n, col0, 0, (UINT) -1, -1, n );
-		ASSERT( -1 != nItem );
+	col1 = VkToString( cke.vkFirst );
+	width = m_KeyComboList.GetStringWidth( col1 ) + ITEM_FUDGE_FACTOR;
+	if ( width > m_nColumnWidths[1] )
+		m_nColumnWidths[1] = width;
+	ret = m_KeyComboList.SetItem( nItem, 1, LVIF_STATE | LVIF_TEXT, col1, -1, 0, (UINT) -1, 0 );
+	ASSERT( TRUE == ret );
 
-		col1 = VkToString( m_Options.m_ComposeKeyEntries[n].vkFirst );
-		width = m_KeyComboList.GetStringWidth( col1 ) + ITEM_FUDGE_FACTOR;
-		if ( width > m_nColumnWidths[1] )
-			m_nColumnWidths[1] = width;
-		ret = m_KeyComboList.SetItem( nItem, 1, LVIF_STATE | LVIF_TEXT, col1, -1, 0, (UINT) -1, 0 );
-		ASSERT( TRUE == ret );
+	col2 = VkToString( cke.vkSecond );
+	width = m_KeyComboList.GetStringWidth( col2 ) + ITEM_FUDGE_FACTOR;
+	if ( width > m_nColumnWidths[2] )
+		m_nColumnWidths[2] = width;
+	ret = m_KeyComboList.SetItem( nItem, 2, LVIF_STATE | LVIF_TEXT, col2, -1, 0, (UINT) -1, 0 );
+	ASSERT( TRUE == ret );
+}
 
-		col2 = VkToString( m_Options.m_ComposeKeyEntries[n].vkSecond );
-		width = m_KeyComboList.GetStringWidth( col2 ) + ITEM_FUDGE_FACTOR;
-		if ( width > m_nColumnWidths[2] )
-			m_nColumnWidths[2] = width;
-		ret = m_KeyComboList.SetItem( nItem, 2, LVIF_STATE | LVIF_TEXT, col2, -1, 0, (UINT) -1, 0 );
-		ASSERT( TRUE == ret );
-	}
-
+void CKeySequences::_AdjustColumns( void ) {
 	int m = max( m_nColumnWidths[1], m_nColumnWidths[2] );
 	m_nColumnWidths[1] = m_nColumnWidths[2] = m;
 	m_KeyComboList.SetColumnWidth( 0, m_nColumnWidths[0] );
 	m_KeyComboList.SetColumnWidth( 1, m_nColumnWidths[1] );
 	m_KeyComboList.SetColumnWidth( 2, m_nColumnWidths[2] );
+}
+
+void CKeySequences::_FillKeyComboList( void ) {
+	m_Options.m_ComposeKeyEntries.RemoveAll( );
+	m_Options.m_ComposeKeyEntries.Copy( ComposeKeyEntries );
+	m_KeyComboList.DeleteAllItems( );
+
+	for ( int n = 0; n < m_Options.m_ComposeKeyEntries.GetCount( ); n++ ) {
+		_DoAddOneKeySequence( n, m_Options.m_ComposeKeyEntries[ n ] );
+	}
+	_AdjustColumns( );
+}
+
+void CKeySequences::_AddNewKeySequence( const COMPOSE_KEY_ENTRY& cke ) {
+	_DoAddOneKeySequence( m_Options.m_ComposeKeyEntries.GetCount( ), cke );
+	_AdjustColumns( );
 }
 
 BOOL CKeySequences::OnInitDialog( ) {
@@ -109,7 +125,35 @@ void CKeySequences::OnKeyComboListItemChanged( NMHDR* pNMHDR, LRESULT* pResult )
 	*pResult = 0;
 }
 
+void CKeySequences::OnBnClickedAdd( ) {
+	COMPOSE_KEY_ENTRY newcke;
+
+	CEditKeySequence edit( newcke, true, this );
+	INT_PTR rc = edit.DoModal( );
+	if ( IDOK == rc ) {
+		m_Options.m_ComposeKeyEntries.Add( newcke );
+		_AddNewKeySequence( newcke );
+		SetModified( );
+	}
+}
+
 void CKeySequences::OnBnClickedEdit( ) {
+	if ( m_KeyComboList.GetSelectedCount( ) < 1 )
+		return;
+
+	POSITION pos = m_KeyComboList.GetFirstSelectedItemPosition( );
+	if ( NULL == pos )
+		return;
+
+	int k = m_KeyComboList.GetNextSelectedItem( pos );
+	COMPOSE_KEY_ENTRY cke( m_Options.m_ComposeKeyEntries[ k ] );
+	CEditKeySequence edit( cke, false, this );
+	INT_PTR rc = edit.DoModal( );
+	if ( IDOK == rc ) {
+		m_Options.m_ComposeKeyEntries[ k ] = cke;
+		m_KeyComboList.Update( k );
+		SetModified( );
+	}
 }
 
 void CKeySequences::OnBnClickedRemove( ) {
