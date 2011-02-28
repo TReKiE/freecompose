304a1290e1bb9a717f203a0310fb346c57b10bc7
icekarma <icekarma@bea30d8e-86d1-9815-9a41-0b703c73c987>
2011-02-28 20:43:59 +0000

    * Move _GetAppDataFolderFromKfm( ) to Utils2.cpp, so just that one file can be built targeting Windows Vista and above.

--- FreeCompose/branches/devel/FreeCompose/Utils.cpp
+++ FreeCompose/branches/devel/FreeCompose/Utils.cpp
@@ -23,40 +23,7 @@ void debug( LPCWSTR /*format*/, ... ) {
 #else
 static FILE* debugFile = NULL;
 
-static bool _GetAppDataFolderFromKfm( CString& str ) {
-	IKnownFolderManager* pkfm = NULL;
-	IKnownFolder* pkf = NULL;
-	LPWSTR path = NULL;
-	bool ret = false;
-	HRESULT hr;
-
-	hr = CoCreateInstance( CLSID_KnownFolderManager, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS( &pkfm ) );
-	if ( FAILED(hr) ) {
-		debug( L"CoCreateInstance(CLSID_KnownFolderManager) failed: %08x\n", hr );
-		goto out1;
-	}
-
-	hr = pkfm->GetFolder( FOLDERID_RoamingAppData, &pkf );
-	if ( FAILED( hr ) ) {
-		debug( L"pkfm->GetFolder(FOLDERID_RoamingAppData) failed: %08x\n", hr );
-		goto out2;
-	}
-
-	hr = pkf->GetPath( 0, &path );
-	if ( FAILED( hr ) ) {
-		debug( L"pkf->GetPath failed: %08x\n", hr );
-		goto out3;
-	}
-
-	str = path;
-	CoTaskMemFree( path );
-
-	ret = true;
-
-out3:	pkf->Release( );
-out2:	pkfm->Release( );
-out1:	return ret;
-}
+extern bool _GetAppDataFolderFromKfm( CString& str );
 
 static bool _GetAppDataFolderFromShell( CString& str ) {
 	wchar_t buf[MAX_PATH];
