d6d71493b14fc93c05b32a379a2237d7d3a6f40c
icekarma <icekarma@gmail.com>
2011-06-05 18:14:50 +0000

    Oops, debug messages didn't include the function name.

--- FreeCompose/branches/devel/FreeCompose/Utils2.cpp
+++ FreeCompose/branches/devel/FreeCompose/Utils2.cpp
@@ -17,19 +17,19 @@ bool _GetAppDataFolderFromKfm( CString& str ) {
 
 	hr = CoCreateInstance( CLSID_KnownFolderManager, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS( &pkfm ) );
 	if ( FAILED(hr) ) {
-		debug( L"CoCreateInstance(CLSID_KnownFolderManager) failed: %08x\n", hr );
+		debug( L"_GetAppDataFolderFromKfm: CoCreateInstance(CLSID_KnownFolderManager) failed: 0x%08x\n", hr );
 		goto out1;
 	}
 
 	hr = pkfm->GetFolder( FOLDERID_RoamingAppData, &pkf );
 	if ( FAILED( hr ) ) {
-		debug( L"pkfm->GetFolder(FOLDERID_RoamingAppData) failed: %08x\n", hr );
+		debug( L"_GetAppDataFolderFromKfm: pkfm->GetFolder(FOLDERID_RoamingAppData) failed: 0x%08x\n", hr );
 		goto out2;
 	}
 
 	hr = pkf->GetPath( 0, &path );
 	if ( FAILED( hr ) ) {
-		debug( L"pkf->GetPath failed: %08x\n", hr );
+		debug( L"_GetAppDataFolderFromKfm: pkf->GetPath failed: 0x%08x\n", hr );
 		goto out3;
 	}
 
