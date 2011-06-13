e41d39a8a55c0d14e5020cc60101ba2f66c43441
icekarma <icekarma@gmail.com>
2011-06-13 19:38:00 +0000

    Bleah, COM api wrapper wasn't such a great idea after all.

--- FreeCompose/branches/devel/FcApi/dllmain.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class CFcApiModule: public ATL::CAtlDllModuleT< CFcApiModule > {
-public:
-	DECLARE_LIBID( LIBID_FcApiLib )
-	DECLARE_REGISTRY_APPID_RESOURCEID( IDR_FCAPI, "{F76B3E97-2B8F-4202-86DE-C71F5980C6F0}" )
-};
-
-extern class CFcApiModule _AtlModule;
--- FreeCompose/branches/devel/FcApi/targetver.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Target Windows 2000 or later.
-#define NTDDI_VERSION  NTDDI_WIN2K
-#define WINVER         _WIN32_WINNT_WIN2K
-#define _WIN32_WINNT   _WIN32_WINNT_WIN2K
-#define _WIN32_IE      _WIN32_IE_IE50
-
-#include <SDKDDKVer.h>
