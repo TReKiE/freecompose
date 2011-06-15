315daeeb75a2b71e8c9b7e2018d881c7831d845b
icekarma <icekarma@gmail.com>
2011-06-15 19:29:29 +0000

    Load configuration on startup.

--- FreeCompose/branches/devel/NewFrontEnd/App.xaml.cs
+++ FreeCompose/branches/devel/NewFrontEnd/App.xaml.cs
@@ -2,13 +2,34 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
+using System.Xml.Linq;
 
 namespace NewFrontEnd {
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App: Application {
+
+        public Configuration Configuration { get; set; }
+
+        void App_Startup( object sender, StartupEventArgs e ) {
+            Debug.Print( "App.App_Startup()" );
+            Configuration = new Configuration( );
+            Configuration.Load( @"C:\Users\jsc\AppData\Roaming\Zive Technology Research\FreeCompose\FreeCompose.xml" );
+        }
+
+        void App_Exit( object sender, ExitEventArgs e ) {
+            Debug.Print( "App.App_Exit()" );
+        }
+
+        void App_SessionEnding( object sender, SessionEndingCancelEventArgs e ) {
+            Debug.Print( "App.App_SessionEnding()" );
+        }
+
     }
+
 }
---
+++ FreeCompose/branches/devel/NewFrontEnd/Configuration.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace NewFrontEnd {
+
+    public class Configuration {
+
+        public Configuration( ) {
+            MappingGroups = new Dictionary<string, MappingGroup>( );
+        }
+
+        Dictionary< string, FcApi.CapsLockToggleModes > mapCltm = new Dictionary< string, FcApi.CapsLockToggleModes > {
+            { "normal",     FcApi.CapsLockToggleModes.Normal     },
+            { "pressTwice", FcApi.CapsLockToggleModes.PressTwice },
+            { "disabled",   FcApi.CapsLockToggleModes.Disabled   },
+        };
+
+        Dictionary< string, FcApi.CapsLockSwapModes > mapClsm = new Dictionary< string, FcApi.CapsLockSwapModes > {
+            { "swap",    FcApi.CapsLockSwapModes.Swap    },
+            { "replace", FcApi.CapsLockSwapModes.Replace },
+        };
+
+        private string getString( XmlElement elt, string name ) {
+            return ( (XmlElement) elt.SelectSingleNode( name ) ).InnerText;
+        }
+
+        public void Load( string fileName ) {
+            try {
+                XmlDocument doc = new XmlDocument( );
+                doc.Load( fileName );
+
+                var root     = doc.SelectSingleNode( "FreeCompose" );
+                var options  = root.SelectSingleNode( "Options" );
+                var mappings = root.SelectSingleNode( "Mappings" );
+                var startup  = (XmlElement) options.SelectSingleNode( "Startup" );
+                var keyboard = (XmlElement) options.SelectSingleNode( "Keyboard" );
+                
+                StartActive        = bool.Parse( getString( startup,  "StartActive"        ) );
+                StartWithWindows   = bool.Parse( getString( startup,  "StartWithWindows"   ) );
+
+                SwapCapsLock       = bool.Parse( getString( keyboard, "SwapCapsLock"       ) );
+                CapsLockToggleMode = mapCltm[    getString( keyboard, "CapsLockToggleMode" ) ];
+                CapsLockSwapMode   = mapClsm[    getString( keyboard, "CapsLockSwapMode"   ) ];
+                ComposeKey         = uint.Parse( getString( keyboard, "ComposeKey"         ) );
+                SwapCapsLockKey    = uint.Parse( getString( keyboard, "SwapCapsLockKey"    ) );
+
+                foreach ( XmlNode nodeGroup in mappings.SelectNodes( "Group" ) ) {
+                    var eltGroup = nodeGroup as XmlElement;
+                    if ( null == eltGroup ) {
+                        continue;
+                    }
+
+                    var group = new MappingGroup { Name = eltGroup.GetAttribute( "Name" ) };
+                    foreach ( XmlNode nodeMapping in eltGroup.SelectNodes( "Mapping" ) ) {
+                        var eltMapping = (XmlElement) nodeMapping;
+                        var Mapping = new FcApi.KeySequence( );
+
+                        var First = getString( eltMapping, "First" );
+                        var FirstShifted = eltMapping.SelectSingleNode( "First/@Shifted" );
+                        var Second = getString( eltMapping, "Second" );
+                        var SecondShifted = eltMapping.SelectSingleNode( "Second/@Shifted" );
+                        var Composed = getString( eltMapping, "Composed" );
+
+                        Mapping.vkFirst = uint.Parse( First );
+                        Mapping.vkSecond = uint.Parse( Second );
+                        Mapping.u32Composed = uint.Parse( Composed );
+                        if ( null != FirstShifted ) {
+                            bool t = bool.Parse( ( (XmlAttribute) FirstShifted ).Value );
+                            if ( t ) {
+                                Mapping.vkFirst |= 0x80000000;
+                            }
+                        }
+                        if ( null != SecondShifted ) {
+                            bool t = bool.Parse( ( (XmlAttribute) SecondShifted ).Value );
+                            if ( t ) {
+                                Mapping.vkSecond |= 0x80000000;
+                            }
+                        }
+
+                        group.KeySequences.Add( Mapping );
+                    }
+
+                    MappingGroups.Add( group.Name, group );
+                }
+            }
+            catch ( Exception e ) {
+                Debug.Print( "Configuration.Load: Exception: {0}", e.ToString( ) );
+                throw e;
+            }
+        }
+
+        public void Save( string fileName ) {
+        }
+
+        public bool StartActive { get; set; }
+        public bool StartWithWindows { get; set; }
+        public bool SwapCapsLock { get; set; }
+        public FcApi.CapsLockToggleModes CapsLockToggleMode { get; set; }
+        public FcApi.CapsLockSwapModes CapsLockSwapMode { get; set; }
+        public uint ComposeKey { get; set; }
+        public uint SwapCapsLockKey { get; set; }
+        public Dictionary<string, MappingGroup> MappingGroups { get; set; }
+    }
+
+    public class MappingGroup {
+        private string name;
+        public string Name {
+            get {
+                return name;
+            }
+            set {
+                if ( string.IsNullOrWhiteSpace( value ) ) {
+                    name = " default";
+                } else {
+                    name = value.Trim( );
+                }
+            }
+        }
+
+        public List< FcApi.KeySequence > KeySequences { get; set; }
+
+        public MappingGroup( ) {
+            KeySequences = new List<FcApi.KeySequence>( );
+        }
+    }
+
+}
