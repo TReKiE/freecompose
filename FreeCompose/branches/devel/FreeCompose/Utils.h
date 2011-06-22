c88e059728c199864f9f7fac12343f3ad7018dc1
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2011-06-22 20:16:05 +0000

    * Add my standard locking constructs, including a modicum of freshly-written docs!

--- FreeCompose/branches/devel/FreeCompose/Utils.h
+++ FreeCompose/branches/devel/FreeCompose/Utils.h
@@ -2,10 +2,32 @@
 
 #define countof(x) ( sizeof((x)) / sizeof((x)[0]) )
 
+//
+// Ice Karma's standard lock constructs. Use like this:
+//
+// LOCK( someCriticalSection ) {
+//     ...
+//     ...
+//     if ( somethingDisagreeable )
+//         break;
+//     ...
+//     ...
+// } UNLOCK( someCriticalSection );
+//
+// Note that you must use the _same_ critical section object in both places!
+// And yes, since it's implemented with a do/while loop, you can cheerfully use
+// "break" to exit a locked region!
+//
+
+#define LOCK(x) EnterCriticalSection(&(x)); do
+#define UNLOCK(x) while ( false ); LeaveCriticalSection(&(x))
+
 void debug( LPCWSTR format, ... );
+
 CString VscToString( DWORD vsc );
 CString VkToString( DWORD vk );
 DWORD VkToVsc( DWORD vk );
+
 CString GetFreeComposeFolder( void );
 bool EnsureFreeComposeFolderExists( void );
 
