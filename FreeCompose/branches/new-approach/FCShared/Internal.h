4f789ed47cfea4655ad0a7be824632615a133481
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2011-07-06 20:13:40 +0000

    * Replaced my countof(x) with Visual Studio's _countof(x) (in stdlib.h). * Added _CoTaskMemDuplicateStringW(), since I cocked up the logic in both places. * Comment cleanup.

--- FreeCompose/branches/new-approach/FCShared/Internal.h
+++ FreeCompose/branches/new-approach/FCShared/Internal.h
@@ -6,6 +6,7 @@
 // company name
 #define AFX_IDS_COMPANY_NAME            0xE004
 
+LPWSTR _CoTaskMemDuplicateStringW( LPCWSTR lpwszSource );
 bool _GetAppDataFolder( LPWSTR& lpsz );
 bool _GetAppDataFolderFromShell( LPWSTR& lpsz );
 bool _GetAppDataFolderFromKfm( LPWSTR& lpsz );
