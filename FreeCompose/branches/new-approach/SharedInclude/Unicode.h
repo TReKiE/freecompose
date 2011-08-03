e4dd83f66826075372db3541bfbe050aa0558837
icekarma <icekarma@gmail.com>
2011-08-03 00:21:32 +0000

    * Broaden suite of functions to accept both wchar_t and unsigned arguments without casts or promotion. * Use new-style placement of "const" keyword.

--- FreeCompose/branches/new-approach/SharedInclude/Unicode.h
+++ FreeCompose/branches/new-approach/SharedInclude/Unicode.h
@@ -1,28 +1,43 @@
 #pragma once
 
-inline bool IsSurrogate( const wchar_t ch ) {
+inline bool IsSurrogate( wchar_t const ch ) {
 	return ( ch >= 0xD800 ) && ( ch < 0xE000 );
 }
 
-inline bool IsFirstSurrogate( const wchar_t ch ) {
+inline bool IsSurrogate( unsigned const ch ) {
+	return ( ch >= 0xD800U ) && ( ch < 0xE000U );
+}
+
+
+inline bool IsFirstSurrogate( wchar_t const ch ) {
 	return ( ch >= 0xD800 ) && ( ch < 0xDC00 );
 }
 
-inline bool IsSecondSurrogate( const wchar_t ch ) {
+inline bool IsFirstSurrogate( unsigned const ch ) {
+	return ( ch >= 0xD800U ) && ( ch < 0xDC00U );
+}
+
+
+inline bool IsSecondSurrogate( wchar_t const ch ) {
 	return ( ch >= 0xDC00 ) && ( ch < 0xE000 );
 }
 
-inline wchar_t MakeFirstSurrogate( unsigned ch ) {
+inline bool IsSecondSurrogate( unsigned const ch ) {
+	return ( ch >= 0xDC00U ) && ( ch < 0xE000U );
+}
+
+
+inline wchar_t MakeFirstSurrogate( unsigned const ch ) {
 	return (wchar_t) ( 0xD800 + ( ( ch - 0x10000 ) >> 10 ) );
 }
 
-inline wchar_t MakeSecondSurrogate( unsigned ch ) {
+inline wchar_t MakeSecondSurrogate( unsigned const ch ) {
 	return (wchar_t) ( 0xDC00 + ( ( ch - 0x10000 ) & 0x3FF ) );
 }
 
 #ifdef __AFXSTR_H__
 
-inline CString Utf32ToUtf16( unsigned ch ) {
+inline CString Utf32ToUtf16( unsigned const ch ) {
 	if ( ch >= 0x10000 ) {
 		wchar_t tmp[3] = {
 			MakeFirstSurrogate( ch ),
@@ -35,7 +50,7 @@ inline CString Utf32ToUtf16( unsigned ch ) {
 	}
 }
 
-inline bool Utf16ToUtf32( CString& s, unsigned& ch ) {
+inline bool Utf16ToUtf32( CString const& s, unsigned& ch ) {
 	wchar_t w1, w2;
 
 	switch ( s.GetLength( ) ) {
