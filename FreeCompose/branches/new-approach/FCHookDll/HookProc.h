438936ba834623410471280d182d5d4180faa177
icekarma <icekarma@gmail.com>
2011-08-07 19:50:05 +0000

    Move ConfigureCapsLockHandling() (né SetUpCapsLockHandling() ) here, and add InitializeKeyEventDispatcher() and ChangeComposeKey().

--- FreeCompose/branches/new-approach/FCHookDll/HookProc.h
+++ FreeCompose/branches/new-approach/FCHookDll/HookProc.h
@@ -1,3 +1,7 @@
 #pragma once
 
 LRESULT CALLBACK LowLevelKeyboardProc( int nCode, WPARAM wParam, LPARAM lParam );
+
+void InitializeKeyEventDispatcher( void );
+void ChangeComposeKey( DWORD const vkNew );
+void ConfigureCapsLockHandling( void );
