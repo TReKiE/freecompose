12a2cca677da6e7544da9dfbb92b5253d81ab918
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2013-04-04 18:28:49 +0000

    * Add new type enum COMPOSE_STATE. * Expose ComposeState, WantedKeys as globals.

--- FreeCompose/branches/new-approach/FCHookDll/HookProc.h
+++ FreeCompose/branches/new-approach/FCHookDll/HookProc.h
@@ -1,12 +1,27 @@
 #pragma once
 
+#include <Bitset.h>
+
 #include "KeyEventHandler.h"
+
+//==============================================================================
+// Types
+//==============================================================================
+
+enum COMPOSE_STATE {
+	csNORMAL,
+	csCOMPOSE_FIRST,
+	csCOMPOSE_SECOND,
+	csCAPS_LOCK_FIRST,
+};
 
 //==============================================================================
 // Globals
 //==============================================================================
 
 extern KeyEventHandler* keyEventHandler[256];
+extern COMPOSE_STATE ComposeState;
+extern zive::bitset< 256, DWORD > WantedKeys;
 
 //==============================================================================
 // Function prototypes
