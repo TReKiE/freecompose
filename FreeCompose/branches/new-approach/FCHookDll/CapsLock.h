773dfd4122998650490fbd176c62c1fbfb922170
icekarma <icekarma@gmail.com>
2013-04-06 19:22:29 +0000

    * Whitespace cleanies. * Make CapsLockKeyHandler::Key{Down,Up} inline functions.

--- FreeCompose/branches/new-approach/FCHookDll/CapsLock.h
+++ FreeCompose/branches/new-approach/FCHookDll/CapsLock.h
@@ -8,18 +8,23 @@ class CapsLockMutator: public KeyEventHandler { };
 
 class CapsLockSwapper: public CapsLockMutator {
 public:
+
 	virtual DISPOSITION KeyDown( KBDLLHOOKSTRUCT* pkb );
 	virtual DISPOSITION KeyUp( KBDLLHOOKSTRUCT* pkb );
+
 };
 
 class CapsLockReplacer: public CapsLockMutator {
 public:
+
 	virtual DISPOSITION KeyDown( KBDLLHOOKSTRUCT* pkb );
 	virtual DISPOSITION KeyUp( KBDLLHOOKSTRUCT* pkb );
+
 };
 
 class CapsLockMutatorFactory {
 public:
+
 	static CapsLockMutator* Create( CAPS_LOCK_SWAP_MODE clSwapMode );
 
 private:
@@ -27,6 +32,7 @@ private:
 	CapsLockMutatorFactory( CapsLockMutatorFactory& );
 	~CapsLockMutatorFactory( );
 	CapsLockMutatorFactory& operator=( CapsLockMutatorFactory& );
+
 };
 
 //
@@ -37,6 +43,7 @@ class CapsLockToggler: public KeyEventHandler { };
 
 class CapsLockPressTwiceToggler: public CapsLockToggler {
 public:
+
 	CapsLockPressTwiceToggler( ): allowDown ( false ), allowUp ( false ) { }
 
 	virtual DISPOSITION KeyDown( KBDLLHOOKSTRUCT* pkb );
@@ -47,16 +54,20 @@ private:
 	bool allowUp;
 
 	DISPOSITION _Implementation( KBDLLHOOKSTRUCT* pkb, bool& allow );
+
 };
 
 class CapsLockDisabledToggler: public CapsLockToggler {
 public:
+
 	virtual DISPOSITION KeyDown( KBDLLHOOKSTRUCT* pkb );
 	virtual DISPOSITION KeyUp( KBDLLHOOKSTRUCT* pkb );
+
 };
 
 class CapsLockTogglerFactory {
 public:
+
 	static CapsLockToggler* Create( CAPS_LOCK_TOGGLE_MODE clToggleMode );
 
 private:
@@ -64,6 +75,7 @@ private:
 	CapsLockTogglerFactory( CapsLockTogglerFactory& );
 	~CapsLockTogglerFactory( );
 	CapsLockTogglerFactory& operator=( CapsLockTogglerFactory& );
+
 };
 
 //
@@ -73,9 +85,10 @@ private:
 class CapsLockKeyHandler: public KeyEventHandler {
 public:
 
-	virtual DISPOSITION KeyDown( KBDLLHOOKSTRUCT* pkb );
-	virtual DISPOSITION KeyUp( KBDLLHOOKSTRUCT* pkb );
+	virtual inline DISPOSITION KeyDown( KBDLLHOOKSTRUCT* pkb ) { return _Implementation( pkb ); }
+	virtual inline DISPOSITION KeyUp( KBDLLHOOKSTRUCT* pkb )   { return _Implementation( pkb ); }
 
 private:
 	DISPOSITION _Implementation( KBDLLHOOKSTRUCT* pkb );
+
 };
