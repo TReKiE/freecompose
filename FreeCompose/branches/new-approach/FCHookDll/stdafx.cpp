5149b9ff826c8aa89c0032d8c22f5f4cf25d4ee4
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2013-04-07 22:00:22 +0000

    * Whoops, forgot to actually move KeyEventHandlers[], WantedKeys, ComposeState here from HookProc.cpp.

--- FreeCompose/branches/new-approach/FCHookDll/stdafx.cpp
+++ FreeCompose/branches/new-approach/FCHookDll/stdafx.cpp
@@ -34,19 +34,25 @@ wchar_t const* stringsFor_enum_COMPOSE_STATE[] = {
 
 #pragma data_seg( push, ".shareddata" )
 
-CRITICAL_SECTION      cs;
-HHOOK                 hHook              = NULL;
+CRITICAL_SECTION           cs;
+HHOOK                      hHook                 = NULL;
 
-COMPOSE_SEQUENCE*     ComposeSequences   = NULL;
-INT_PTR               cComposeSequences  = 0;
+COMPOSE_SEQUENCE*          ComposeSequences      = NULL;
+INT_PTR                    cComposeSequences     = 0;
 
-CAPS_LOCK_TOGGLE_MODE clToggleMode       = CLTM_NORMAL;
-CAPS_LOCK_SWAP_MODE   clSwapMode         = CLSM_SWAP;
-bool                  fSwapCapsLock      = false;
+CAPS_LOCK_TOGGLE_MODE      clToggleMode          = CLTM_NORMAL;
+CAPS_LOCK_SWAP_MODE        clSwapMode            = CLSM_SWAP;
+bool                       fSwapCapsLock         = false;
 
-DWORD                 vkCompose          = VK_APPS;
-DWORD                 vkCapsLockSwap     = VK_CAPITAL;
+DWORD                      vkCompose             = VK_APPS;
+DWORD                      vkCapsLockSwap        = VK_CAPITAL;
 
-HINSTANCE             hDllInst           = NULL;
+KeyEventHandler*           KeyEventHandlers[256] = { NULL, };
+
+zive::bitset< 256, DWORD > WantedKeys;
+
+COMPOSE_STATE              ComposeState          = csNORMAL;
+
+HINSTANCE                  hDllInst              = NULL;
 
 #pragma data_seg( pop )
