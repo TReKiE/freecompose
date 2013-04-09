384d90a52f68893a968cf7a1111318cc87aa4c57
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2013-04-09 04:12:20 +0000

    * Expand handling in KeyDown() to be aware of the current state machine state.

--- FreeCompose/branches/new-approach/FCHookDll/ComposeKeyHandler.cpp
+++ FreeCompose/branches/new-approach/FCHookDll/ComposeKeyHandler.cpp
@@ -6,9 +6,25 @@
 #include "ComposeKeyHandler.h"
 
 DISPOSITION ComposeKeyHandler::KeyDown( KBDLLHOOKSTRUCT* /*pkb*/ ) {
-	WantedKeys.Add( vkCompose );
-	ComposeState = csCOMPOSE_FIRST;
-	return D_REJECT_KEY;
+	switch ( ComposeState ) {
+		case csNORMAL:
+			ComposeState = csCOMPOSE_FIRST;
+			WantedKeys.Add( vkCompose );
+			if ( hwndNotifyWindow ) {
+				::PostMessage( hwndNotifyWindow, FCM_PIP, PIP_OK_1, 0 );
+			}
+			return D_REJECT_KEY;
+
+		case csCOMPOSE_FIRST:
+			ComposeState = csNORMAL;
+			WantedKeys.Remove( vkCompose );
+			if ( hwndNotifyWindow ) {
+				::PostMessage( hwndNotifyWindow, FCM_PIP, PIP_ABORT, 0 );
+			}
+			return D_ACCEPT_KEY;
+	}
+
+	return D_NOT_HANDLED;
 }
 
 DISPOSITION ComposeKeyHandler::KeyUp( KBDLLHOOKSTRUCT* /*pkb*/ ) {
