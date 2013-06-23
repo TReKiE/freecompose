e099c3114a567453f4785fa76bf8b4774b981b89
icekarma <icekarma@gmail.com>
2013-06-23 17:59:36 +0000

    * Remove WantedKeys, instead using KeyEventHandlers[] and KeyUpSink as they were intended. * Remove unnecessary method ComposeKeyHandler::KeyUp().

--- FreeCompose/branches/new-approach/FCHookDll/ComposeKeyHandler.cpp
+++ FreeCompose/branches/new-approach/FCHookDll/ComposeKeyHandler.cpp
@@ -3,13 +3,14 @@
 #include "FCHookDll.h"
 #include "Common.h"
 #include "KeyEventHandler.h"
+#include "KeyUpSink.h"
 #include "ComposeKeyHandler.h"
 
-DISPOSITION ComposeKeyHandler::KeyDown( KBDLLHOOKSTRUCT* /*pkb*/ ) {
+DISPOSITION ComposeKeyHandler::KeyDown( KBDLLHOOKSTRUCT* pkb ) {
 	switch ( ComposeState ) {
 		case csNORMAL:
 			ComposeState = csCOMPOSE_FIRST;
-			WantedKeys.Add( vkCompose );
+			KeyEventHandlers[ vkCompose ] = new KeyUpSink( pkb->vkCode, this );
 			if ( hwndNotifyWindow ) {
 				::PostMessage( hwndNotifyWindow, FCM_PIP, PIP_OK_1, 0 );
 			}
@@ -17,16 +18,12 @@ DISPOSITION ComposeKeyHandler::KeyDown( KBDLLHOOKSTRUCT* /*pkb*/ ) {
 
 		case csCOMPOSE_FIRST:
 			ComposeState = csNORMAL;
-			WantedKeys.Remove( vkCompose );
+			KeyEventHandlers[ vkCompose ] = new KeyUpSink( pkb->vkCode, this );
 			if ( hwndNotifyWindow ) {
 				::PostMessage( hwndNotifyWindow, FCM_PIP, PIP_ABORT, 0 );
 			}
 			return D_ACCEPT_KEY;
 	}
 
-	return D_NOT_HANDLED;
-}
-
-DISPOSITION ComposeKeyHandler::KeyUp( KBDLLHOOKSTRUCT* /*pkb*/ ) {
 	return D_NOT_HANDLED;
 }
