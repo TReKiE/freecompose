1c4cd503acce393cc37a5b0f92b3438fd8b185df
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2013-06-29 22:56:10 +0000

    * Convert class Stringify into a namespace, in order to allow the addition of arbitrary 'members' close to the definition of their related types. * Except for type bool (currently), relocate from_T functions and associated string tables nearer to the definitions of T.

--- FreeCompose/branches/new-approach/FCHookDll/KeyEventHandler.h
+++ FreeCompose/branches/new-approach/FCHookDll/KeyEventHandler.h
@@ -1,5 +1,9 @@
 #pragma once
 
+//==============================================================================
+// Data types
+//==============================================================================
+
 enum DISPOSITION {
 	D_NOT_HANDLED,
 	D_ACCEPT_KEY,
@@ -7,10 +11,24 @@ enum DISPOSITION {
 	D_REGENERATE_KEY,
 };
 
+//==============================================================================
+// Classes and functions
+//==============================================================================
+
 class KeyEventHandler {
 public:
 
 	virtual DISPOSITION KeyDown( KBDLLHOOKSTRUCT* /*pkb*/ );
 	virtual DISPOSITION KeyUp( KBDLLHOOKSTRUCT* /*pkb*/ );
+
+};
+
+namespace Stringify {
+
+	extern wchar_t const* stringsFor_enum_DISPOSITION[];
+
+	static inline wchar_t const* from_DISPOSITION( DISPOSITION const value ) {
+		return stringsFor_enum_DISPOSITION[static_cast<int>( value )];
+	}
 
 };
