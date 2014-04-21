f5581f906f2faf82938555f0178081aef933024e
icekarma <icekarma@gmail.com>
2014-04-21 01:33:06 +0000

    * Whitespace and formatting cleanies. * C++11 cleanies (e.g., nullptr instead of NULL). * Much code cleaning. * Fix printf() format strings for GetLastError(): should be %lu, not %d. * Replace str.Format/OutputDebugString pairs with debug().

--- FreeCompose/branches/new-approach/FreeCompose/AboutDlg.cpp
+++ FreeCompose/branches/new-approach/FreeCompose/AboutDlg.cpp
@@ -5,52 +5,59 @@
 #include "FreeCompose.h"
 #include "AboutDlg.h"
 
-BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
-END_MESSAGE_MAP()
+BEGIN_MESSAGE_MAP( CAboutDlg, CDialog )
+END_MESSAGE_MAP( )
 
 CAboutDlg::CAboutDlg( ):
-	CDialog ( CAboutDlg::IDD )
+	CDialog( CAboutDlg::IDD )
 {
+
 }
 
 void CAboutDlg::DoDataExchange( CDataExchange* pDX ) {
 	CDialog::DoDataExchange( pDX );
-	DDX_Text( pDX, IDC_ABOUT_VERSION, m_strVersion );
+	DDX_Text( pDX, IDC_ABOUT_VERSION,   m_strVersion   );
 	DDX_Text( pDX, IDC_ABOUT_COPYRIGHT, m_strCopyright );
 }
 
 BOOL CAboutDlg::OnInitDialog( ) {
-	void* pvdata = NULL;
-	CString str;
 	CString strExeName;
-	DWORD dwBytes, dwDummy;
-
 	strExeName.Format( L"%s.exe", theApp.m_pszExeName );
-	dwBytes = GetFileVersionInfoSize( strExeName, &dwDummy );
+	
+	SetLastError( ERROR_SUCCESS );
+	DWORD dwBytes = GetFileVersionInfoSize( strExeName, nullptr/*&dwDummy*/ );
 	if ( 0 == dwBytes ) {
-		str.Format( L"GetFileVersionInfoSize failed: %d\n", GetLastError() );
-		OutputDebugString( str );
+		DWORD dwError = GetLastError( );
+		debug( L"GetFileVersionInfoSize failed: %lu\n", dwError );
 		return FALSE;
 	}
-	pvdata = malloc( dwBytes );
-	if ( ! GetFileVersionInfo( strExeName, NULL, dwBytes, pvdata ) ) {
-		str.Format( L"GetFileVersionInfo failed: %d\n", GetLastError() );
-		OutputDebugString( str );
+
+	SetLastError( ERROR_SUCCESS );
+	void* pvdata = malloc( dwBytes );
+	if ( !pvdata ) {
+		DWORD dwError = GetLastError( );
+		debug( L"CAboutDlg::InitDialog: malloc(%lu) failed, errno: %lu, Win32 error: %lu\n", dwBytes, errno, dwError );
+		return FALSE;
+	}
+
+	SetLastError( ERROR_SUCCESS );
+	if ( !GetFileVersionInfo( strExeName, 0, dwBytes, pvdata ) ) {
+		DWORD dwError = GetLastError( );
+		debug( L"GetFileVersionInfo failed: %lu\n", dwError );
 		free( pvdata );
 		return FALSE;
 	}
 
-	VS_FIXEDFILEINFO* pvsffi = NULL;
-	LPVOID pvffi = NULL;
-	UINT uLen;
-	if ( ! VerQueryValue( pvdata, L"\\", &pvffi, &uLen ) ) {
-		str.Format( L"VerQueryValue failed: %d\n", GetLastError() );
-		OutputDebugString( str );
+	VS_FIXEDFILEINFO* pvsffi = nullptr;
+	UINT uLen = 0;
+	SetLastError( ERROR_SUCCESS );
+	if ( !VerQueryValue( pvdata, L"\\", reinterpret_cast<LPVOID*>( &pvsffi ), &uLen ) ) {
+		DWORD dwError = GetLastError( );
+		debug( L"VerQueryValue failed: %lu\n", dwError );
 		free( pvdata );
 		return FALSE;
 	}
-	pvsffi = (VS_FIXEDFILEINFO*) pvffi;
-	
+
 	m_strVersion.Format(
 		CString( (LPCWSTR) IDS_ABOUT_VERSION ),
 		CString( (LPCWSTR) AFX_IDS_APP_TITLE ),
@@ -58,9 +65,7 @@ BOOL CAboutDlg::OnInitDialog( ) {
 		HIWORD( pvsffi->dwProductVersionLS ), LOWORD( pvsffi->dwProductVersionLS )
 	);
 
-	if ( NULL != pvdata ) {
-		free( pvdata );
-	}
+	free( pvdata );
 	UpdateData( FALSE );
 	return TRUE;
 }
