8cac534ce63a3349d168fb21f7ce1517c6526c49
icekarma <icekarma@gmail.com>
2014-04-22 17:10:17 +0000

    * VS2013's MFC no longer supports Windows 2000.

--- FreeCompose/branches/new-approach/FreeCompose/targetver.h
+++ FreeCompose/branches/new-approach/FreeCompose/targetver.h
@@ -8,17 +8,11 @@
 #include <winsdkver.h>
 
 // Target Windows XP or later.
-// (We still support Windows 2000, but we need to target XP in order to make some #defines available.)
-// Let's see if the above comment about #defines is actually the case...
+// (Support for Windows 2000 is a casualty of MFC no longer supporting it.)
 
-#define _WIN32_WINNT  _WIN32_WINNT_WIN2K
-#define NTDDI_VERSION NTDDI_WIN2K
-#define WINVER        _WIN32_WINNT_WIN2K
-#define _WIN32_IE     _WIN32_IE_WIN2K
-
-//#define _WIN32_WINNT _WIN32_WINNT_WINXP
-//#define NTDDI_VERSION NTDDI_WINXP
-//#define WINVER        _WIN32_WINNT_WINXP
-//#define _WIN32_IE     _WIN32_IE_WINXP
+#define _WIN32_WINNT _WIN32_WINNT_WINXP
+#define NTDDI_VERSION NTDDI_WINXP
+#define WINVER        _WIN32_WINNT_WINXP
+#define _WIN32_IE     _WIN32_IE_WINXP
 
 #include <SDKDDKVer.h>
