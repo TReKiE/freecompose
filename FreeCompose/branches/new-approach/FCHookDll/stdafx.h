0e2bf9a3ed9f0dd991c618091394e1bd911c30aa
icekarma <icekarma@gmail.com>
2014-04-24 17:02:20 +0000

    * Describe the purpose of the project build control #defines. * Move DEBUG_NODES_LEAVES here. * Add _CRTDBG_MAP_ALLOC and include memory management-related headers.

--- FreeCompose/branches/new-approach/FCHookDll/stdafx.h
+++ FreeCompose/branches/new-approach/FCHookDll/stdafx.h
@@ -1,7 +1,15 @@
 #pragma once
 
-//#define UI_DEBUGGING_ONLY 1
+// Control whether to disable all backend functionality so the UI may be debugged in relative isolation.
 #undef UI_DEBUGGING_ONLY
+
+// Control certain debug messages from COMPOSE_SEQUENCE_TREE and COMPOSE_TREE_NODE.
+#undef DEBUG_NODES_LEAVES
+
+// Enable redirection of malloc, free, etc. to _malloc_dbg, _free_dbg, etc.
+#ifdef _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#endif
 
 #include "targetver.h"
 
@@ -9,6 +17,9 @@
 
 // C standard library
 #include <cctype>
+#include <cstdlib>
+#include <crtdbg.h>
+#include <malloc.h>
 
 // C++ standard library
 #include <algorithm>
