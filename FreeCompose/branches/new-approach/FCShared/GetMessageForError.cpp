d72dac6fec9c02d9ece57124ebb97131ef08bdd5
icekarma <icekarma@gmail.com>
2014-04-25 15:39:20 +0000

    * Add new files GetMessageForError.cpp, GetMessageForError.h, which contains new function GetMessageForError().

---
+++ FreeCompose/branches/new-approach/FCShared/GetMessageForError.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+
+#include "FCShared.h"
+#include "GetMessageForError.h"
+
+wchar_t* GetMessageForError( DWORD const dwError ) {
+	wchar_t* buf = nullptr;
+	DWORD rc = FormatMessage( FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, dwError, 0, reinterpret_cast<LPWSTR>( &buf ), 0, nullptr );
+	if ( !rc ) {
+		debug( L"GetMessageForError: FormatMessage for error code %d failed, error %d\n", dwError, GetLastError( ) );
+	}
+	return buf;
+}
