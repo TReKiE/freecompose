0cb392f29ef8613bf3f4c55764e960c762b931a7
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-04-25 15:09:28 +0000

    * Adjust target Windows platform to XP because MFC no longer supports Windows 2000.

--- FreeCompose/branches/new-approach/FCHookDll/targetver.h
+++ FreeCompose/branches/new-approach/FCHookDll/targetver.h
@@ -1,15 +1,10 @@
 #pragma once
 
-// Including SDKDDKVer.h defines the highest available Windows platform.
-
-// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
-// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
-
 #include <winsdkver.h>
 
-#define NTDDI_VERSION  NTDDI_WIN2K
-#define WINVER         _WIN32_WINNT_WIN2K
-#define _WIN32_WINNT   _WIN32_WINNT_WIN2K
-#define _WIN32_IE      _WIN32_IE_IE50
+#define _WIN32_WINNT _WIN32_WINNT_WINXP
+#define NTDDI_VERSION NTDDI_WINXP
+#define WINVER        _WIN32_WINNT_WINXP
+#define _WIN32_IE     _WIN32_IE_WINXP
 
 #include <SDKDDKVer.h>
--- FreeCompose/branches/new-approach/FCShared/targetver_vista.h
+++ FreeCompose/branches/new-approach/FCShared/targetver_vista.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <winsdkver.h>
+
 #define NTDDI_VERSION  NTDDI_VISTA
 #define WINVER         _WIN32_WINNT_VISTA
 #define _WIN32_WINNT   _WIN32_WINNT_VISTA
-#define _WIN32_IE      _WIN32_IE_IE60
+#define _WIN32_IE      _WIN32_IE_VISTA
 
 #include <SDKDDKVer.h>
