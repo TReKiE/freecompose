bec7d3f5b016ab287586b46279e41917885fbea8
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-05-04 20:00:31 +0000

    * Add new dialog IDD_EDITKEYSEQUENCE2, a mockup.

--- FreeCompose/branches/new-approach/FreeCompose/resource.h
+++ FreeCompose/branches/new-approach/FreeCompose/resource.h
@@ -10,6 +10,13 @@
 #define IDD_FEATURES                    131
 #define IDD_EDITKEYSEQUENCE             132
 #define IDD_SOUNDS                      133
+#define IDD_EDITKEYSEQUENCE2            134
+#define IDC_EDIT1                       1001
+#define IDC_RESULT_ASCHARACTER          1002
+#define IDC_RESULT_ASHEXCODEPOINT       1003
+#define IDC_RESULT_ASDECCODEPOINT       1004
+#define IDC_EDIT2                       1005
+#define IDC_EDITRESULT                  1005
 #define IDC_ABOUT_VERSION               2001
 #define IDC_ABOUT_COPYRIGHT             2002
 #define IDC_KEYCOMBOLIST                3001
@@ -69,9 +76,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_RESOURCE_VALUE        135
 #define _APS_NEXT_COMMAND_VALUE         32773
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
