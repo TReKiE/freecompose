68aaf40b32319c43904a20403190f7ca572519cf
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-05-04 10:31:42 +0000

    * Replace switch statement with ColumnSortFuncMap lookup. * Reset m_pnSortIndices to nullptr when not in use. * Another place to use SetRedraw? * Add NumberOfColumns, use it instead of literal 3 where appropriate, and rearrange the class declaration some.

--- FreeCompose/branches/new-approach/FreeCompose/KeySequences.h
+++ FreeCompose/branches/new-approach/FreeCompose/KeySequences.h
@@ -2,7 +2,7 @@
 
 #include "OptionsData.h"
 
-enum SORTSTATE { ssUnsorted, ssAscending, ssDescending };
+enum SORTSTATE { ssUnsorted, ssAscending, ssDescending, ssMaximumValue };
 
 class CKeySequences: public CPropertyPage {
 	DECLARE_DYNAMIC( CKeySequences )
@@ -26,9 +26,20 @@ public:
 	afx_msg void OnBnClickedRemove( );
 
 private:
+	static int const ResultCodePointColumn = 0;
+	static int const ResultCharacterColumn = 1;
+	static int const SequenceColumn = 2;
+	static int const NumberOfColumns = 3;
+
+	using sortcallbackfunc = int CALLBACK ( LPARAM, LPARAM, LPARAM );
+	static sortcallbackfunc* ResultColumnsSortFuncMap[3];  // 3 is the number of SORTSTATEs, NOT the number of columns!
+	static sortcallbackfunc* SequenceColumnSortFuncMap[3]; // 3 is the number of SORTSTATEs, NOT the number of columns!
+	static sortcallbackfunc** ColumnSortFuncMap[NumberOfColumns];
+
+private:
 	COptionsData& m_Options;
 
-	int m_nColumnWidths[3];
+	int m_nColumnWidths[NumberOfColumns];
 
 	CListCtrl m_List;
 	CHeaderCtrl* m_pListHeader;
@@ -40,6 +51,7 @@ private:
 	int m_nSortColumn;
 	int* m_pnSortIndices;
 
+private:
 	CString _FormatResultString( ComposeSequence const& sequence );
 	void _AddOneKeySequence( const INT_PTR n );
 	void _UpdateOneKeySequence( const INT_PTR n );
@@ -48,18 +60,11 @@ private:
 	void _AddNewKeySequence( const INT_PTR n );
 	void _SetColumnSortState( int nColumn, SORTSTATE state );
 
+private:
 	static int CALLBACK _ListComparer_Unsorted( LPARAM index1, LPARAM index2, LPARAM );
 	static int CALLBACK _ListComparer_Ascending_Result( LPARAM index1, LPARAM index2, LPARAM );
 	static int CALLBACK _ListComparer_Descending_Result( LPARAM index1, LPARAM index2, LPARAM );
 	static int CALLBACK _ListComparer_Ascending_Sequence( LPARAM index1, LPARAM index2, LPARAM );
 	static int CALLBACK _ListComparer_Descending_Sequence( LPARAM index1, LPARAM index2, LPARAM );
-
-	using sortcallbackfunc = int CALLBACK ( LPARAM, LPARAM, LPARAM );
-	static sortcallbackfunc* ResultColumnsSortFuncMap[3];
-	static sortcallbackfunc* SequenceColumnSortFuncMap[3];
-
-	static int const ResultCodePointColumn = 0;
-	static int const ResultCharacterColumn = 1;
-	static int const SequenceColumn = 2;
 
 };
