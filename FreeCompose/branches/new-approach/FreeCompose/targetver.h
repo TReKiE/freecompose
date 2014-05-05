965f36606a55eab3aee9107daa6afd979c21a086
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-05-05 16:04:04 +0000

    * Add WindowsMajorVersion, WindowsMinorVersion, WindowsServicePackMajor, derived from NTDDI_VERSION.

--- FreeCompose/branches/new-approach/FreeCompose/targetver.h
+++ FreeCompose/branches/new-approach/FreeCompose/targetver.h
@@ -5,9 +5,13 @@
 // Target Windows XP or later.
 // (Support for Windows 2000 is a casualty of MFC no longer supporting it.)
 
-#define _WIN32_WINNT _WIN32_WINNT_WINXP
+#define _WIN32_WINNT  _WIN32_WINNT_WINXP
 #define NTDDI_VERSION NTDDI_WINXP
 #define WINVER        _WIN32_WINNT_WINXP
 #define _WIN32_IE     _WIN32_IE_WINXP
 
 #include <SDKDDKVer.h>
+
+unsigned short const WindowsMajorVersion     = static_cast<unsigned short>( ( NTDDI_VERSION >> 24 ) & 0x000000FF );
+unsigned short const WindowsMinorVersion     = static_cast<unsigned short>( ( NTDDI_VERSION >> 16 ) & 0x000000FF );
+unsigned short const WindowsServicePackMajor = static_cast<unsigned short>( ( NTDDI_VERSION >>  8 ) & 0x000000FF );
