43f0a9b8fca677ac42f309558edcd7a3205b63c2
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-05-06 17:53:40 +0000

    * More cleanies.

--- FreeCompose/branches/new-approach/FCShared/targetver.h
+++ FreeCompose/branches/new-approach/FCShared/targetver.h
@@ -2,7 +2,7 @@
 
 #include <winsdkver.h>
 
-#define _WIN32_WINNT _WIN32_WINNT_WINXP
+#define _WIN32_WINNT  _WIN32_WINNT_WINXP
 #define NTDDI_VERSION NTDDI_WINXP
 #define WINVER        _WIN32_WINNT_WINXP
 #define _WIN32_IE     _WIN32_IE_WINXP
--- FreeCompose/branches/new-approach/FCShared/targetver_vista.h
+++ FreeCompose/branches/new-approach/FCShared/targetver_vista.h
@@ -2,9 +2,9 @@
 
 #include <winsdkver.h>
 
-#define NTDDI_VERSION  NTDDI_VISTA
-#define WINVER         _WIN32_WINNT_VISTA
-#define _WIN32_WINNT   _WIN32_WINNT_VISTA
-#define _WIN32_IE      _WIN32_IE_VISTA
+#define _WIN32_WINNT  _WIN32_WINNT_VISTA
+#define NTDDI_VERSION NTDDI_VISTA
+#define WINVER        _WIN32_WINNT_VISTA
+#define _WIN32_IE     _WIN32_IE_VISTA
 
 #include <SDKDDKVer.h>
