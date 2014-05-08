253397328b852d57aa6445518c40239ef3a92114
icekarma <icekarma@gmail.com>
2014-05-08 16:02:04 +0000

    * Refactor a bit.

--- FreeCompose/branches/new-approach/FreeCompose/AppSoundsRegistry.h
+++ FreeCompose/branches/new-approach/FreeCompose/AppSoundsRegistry.h
@@ -5,4 +5,9 @@ public:
 
 	static void RegisterFcAppSounds( void );
 
+private:
+
+	static void _RegisterEventLabels( wchar_t const* pwzExeName );
+	static void _RegisterApp( wchar_t const* pwzExeName );
+
 };
