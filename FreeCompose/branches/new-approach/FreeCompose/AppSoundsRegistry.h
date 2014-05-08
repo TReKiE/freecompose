9901b1b25b7f322d79bb0866e114ef0046ea5f14
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-05-08 16:53:36 +0000

    * Rearrange file a bit. * Add new public method UnregisterFcAppSounds(), and private methods _UnregisterEventLabels(), _UnregisterApp().

--- FreeCompose/branches/new-approach/FreeCompose/AppSoundsRegistry.h
+++ FreeCompose/branches/new-approach/FreeCompose/AppSoundsRegistry.h
@@ -4,10 +4,14 @@ class CAppSoundsRegistry {
 public:
 
 	static void RegisterFcAppSounds( void );
+	static void UnregisterFcAppSounds( void );
 
 private:
 
 	static void _RegisterEventLabels( wchar_t const* pwzExeName );
 	static void _RegisterApp( wchar_t const* pwzExeName );
+
+	static void _UnregisterEventLabels( void );
+	static void _UnregisterApp( void );
 
 };
