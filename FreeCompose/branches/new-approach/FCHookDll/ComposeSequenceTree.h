a54367f6df9c59ce855d7426ade543bc6b062c43
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-05-08 18:42:02 +0000

    * Sprinkle 'const' on things, and add one const_cast<>. * Add _Validate{Sequence,Result}(). * Rename _AddOneSequence() to _AddSequence(). * LookUp: Remove local variable cchSequence, but add local variable length.

--- FreeCompose/branches/new-approach/FCHookDll/ComposeSequenceTree.h
+++ FreeCompose/branches/new-approach/FCHookDll/ComposeSequenceTree.h
@@ -15,7 +15,33 @@ public:
 	}
 
 private:
-	void _AddOneSequence( CString& strSequence, CString& strResult ) {
+	bool _ValidateSequence( CString const& strSequence ) {
+		int cchSequence = strSequence.GetLength( );
+		if ( cchSequence < 2 ) {
+			debug( L"CComposeSequenceTree::LookUp: character sequence is too short\n" );
+			return false;
+		} else if ( cchSequence > MAXIMUM_SEQUENCE_LENGTH ) {
+			debug( L"CComposeSequenceTree::LookUp: character sequence is too long\n" );
+			return false;
+		}
+
+		return true;
+	}
+
+	bool _ValidateResult( CString const& strResult ) {
+		int cchResult = strResult.GetLength( );
+		if ( cchResult < 1 ) {
+			debug( L"CComposeSequenceTree::_ValidateSequence: result string is too short\n" );
+		}
+		// TODO
+		// add more tests against strResult, like
+		// -- is it a regular character or combining character, followed by zero or more combining characters?
+		//    -- that is to say, does it produce a single, optionally-modified, glyph?
+
+		return true;
+	}
+
+	void _AddSequence( CString const& strSequence, CString const& strResult ) {
 		CComposeTreeNode* pNode = _pRoot;
 		int n;
 
@@ -32,52 +58,58 @@ private:
 			pNode = pNext;
 		}
 
-		pNode->AddLeaf( strSequence[n], strResult );
+		pNode->AddLeaf( strSequence[n], const_cast<CString&>( strResult ) );
 	}
 
 public:
-	void BuildTree( ComposeSequence* pSequences, INT_PTR cSequences ) {
+	void BuildTree( ComposeSequence const* pSequences, INT_PTR cSequences ) {
 		ReleaseTree( );
 		_pRoot = new CComposeTreeNode( );
 
 		for ( INT_PTR index = 0; index < cSequences; index++ ) {
-			CString& sequence = pSequences[index].Sequence;
-			CString& result = pSequences[index].Result;
+			CString const& sequence = pSequences[index].Sequence;
+			CString const& result = pSequences[index].Result;
 			int cchSequence = sequence.GetLength( );
 
-			_AddOneSequence( sequence, result );
+			if ( !_ValidateSequence( sequence ) ) {
+				debug( L"CComposeSequenceTree::BuildTree: compose sequence #%d: key sequence is invalid\n", index );
+				continue;
+			}
+			if ( !_ValidateResult( result ) ) {
+				debug( L"CComposeSequenceTree::BuildTree: compose sequence #%d: result is invalid\n", index );
+				continue;
+			}
+
+			_AddSequence( sequence, result );
 
 			if ( 2 == cchSequence ) {
 				// for backwards compatibility, if a sequence is two characters long, add its reverse as well
 				wchar_t ch[2] = { sequence[0], sequence[1] };
 				CString swapped( ch, 2 );
 
-				_AddOneSequence( swapped, result );
+				_AddSequence( swapped, result );
 			}
 		}
 	}
 
 	bool LookUp( CString const& strSequence, CString& strResult ) {
-		int cchSequence = strSequence.GetLength( );
-		debug( L"CComposeSequenceTree::LookUp: length=%d characters='%s'\n", cchSequence, strSequence.GetString( ) );
+		debug( L"CComposeSequenceTree::LookUp: length=%d characters='%s'\n", strSequence.GetLength( ), strSequence.GetString( ) );
+
 		strResult.Empty( );
 
-		if ( cchSequence < 2 ) {
-			debug( L"CComposeSequenceTree::LookUp: character sequence is too short\n" );
-			return false;
-		}
-		if ( cchSequence > MAXIMUM_SEQUENCE_LENGTH ) {
-			debug( L"CComposeSequenceTree::LookUp: character sequence is too long\n" );
+		if ( !_ValidateSequence( strSequence ) ) {
+			debug( L"CComposeSequenceTree::LookUp: key sequence is invalid\n" );
 			return false;
 		}
 
+		int length = strSequence.GetLength( );
 		CComposeTreeNode* node = _pRoot;
 		int n;
 
-		for ( n = 0; n < cchSequence - 1; n++ ) {
+		for ( n = 0; n < length - 1; n++ ) {
 			CComposeTreeNode* next = node->GetChild( strSequence[n] );
 			if ( !next ) {
-				debug( L"CComposeSequenceTree::LookUp: Hit bottom of tree before we should've. node=0x%p length=%d index=%d\n", node, cchSequence, n );
+				debug( L"CComposeSequenceTree::LookUp: Hit bottom of tree before we should've. node=0x%p length=%d index=%d\n", node, length, n );
 				return false;
 			}
 			node = next;
@@ -89,7 +121,7 @@ public:
 		}
 
 		if ( !node->GetLeaf( strSequence[n], strResult ) ) {
-			debug( L"CComposeSequenceTree::LookUp: Leaf lookup failed. node=0x%p length=%d index=%d\n", node, cchSequence, n );
+			debug( L"CComposeSequenceTree::LookUp: Leaf lookup failed. node=0x%p length=%d index=%d\n", node, length, n );
 			return false;
 		}
 
