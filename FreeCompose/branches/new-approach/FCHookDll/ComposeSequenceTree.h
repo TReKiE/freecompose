a748ed23baa61ba69d65c096a1ee8949211d4256
icekarma <icekarma@gmail.com>
2014-05-08 17:33:42 +0000

    * Replace last few wchar_t[] with CString. * This means we don't need to pass around string lengths any more, either (not that we *really* did, to start with).

--- FreeCompose/branches/new-approach/FCHookDll/ComposeSequenceTree.h
+++ FreeCompose/branches/new-approach/FCHookDll/ComposeSequenceTree.h
@@ -15,10 +15,11 @@ public:
 	}
 
 private:
-	void _AddOneSequence( int const cchSequence, CString& strSequence, CString& strResult ) {
+	void _AddOneSequence( CString& strSequence, CString& strResult ) {
 		CComposeTreeNode* pNode = _pRoot;
 		int n;
 
+		int cchSequence = strSequence.GetLength( );
 		for ( n = 0; n < cchSequence - 1; n++ ) {
 			wchar_t ch = strSequence[n];
 
@@ -44,20 +45,21 @@ public:
 			CString& result = pSequences[index].Result;
 			int cchSequence = sequence.GetLength( );
 
-			_AddOneSequence( cchSequence, sequence, result );
+			_AddOneSequence( sequence, result );
 
 			if ( 2 == cchSequence ) {
 				// for backwards compatibility, if a sequence is two characters long, add its reverse as well
 				wchar_t ch[2] = { sequence[0], sequence[1] };
 				CString swapped( ch, 2 );
 
-				_AddOneSequence( cchSequence, swapped, result );
+				_AddOneSequence( swapped, result );
 			}
 		}
 	}
 
-	bool LookUp( int cchSequence, wchar_t const wchSequence[], CString& strResult ) {
-		debug( L"CComposeSequenceTree::LookUp: length=%d characters={%.*s}\n", cchSequence, cchSequence, wchSequence );
+	bool LookUp( CString const& strSequence, CString& strResult ) {
+		int cchSequence = strSequence.GetLength( );
+		debug( L"CComposeSequenceTree::LookUp: length=%d characters='%s'\n", cchSequence, strSequence.GetString( ) );
 		strResult.Empty( );
 
 		if ( cchSequence < 2 ) {
@@ -73,12 +75,7 @@ public:
 		int n;
 
 		for ( n = 0; n < cchSequence - 1; n++ ) {
-			if ( !wchSequence[n] ) {
-				debug( L"CComposeSequenceTree::LookUp: Run out of characters before we should've. node=0x%p length=%d index=%d\n", node, cchSequence, n );
-				return false;
-			}
-
-			CComposeTreeNode* next = node->GetChild( wchSequence[n] );
+			CComposeTreeNode* next = node->GetChild( strSequence[n] );
 			if ( !next ) {
 				debug( L"CComposeSequenceTree::LookUp: Hit bottom of tree before we should've. node=0x%p length=%d index=%d\n", node, cchSequence, n );
 				return false;
@@ -91,7 +88,7 @@ public:
 			return false;
 		}
 
-		if ( !node->GetLeaf( wchSequence[n], strResult ) ) {
+		if ( !node->GetLeaf( strSequence[n], strResult ) ) {
 			debug( L"CComposeSequenceTree::LookUp: Leaf lookup failed. node=0x%p length=%d index=%d\n", node, cchSequence, n );
 			return false;
 		}
