006f2a808c4e215707041a6b8ec4e09cbc288c8f
icekarma <icekarma@gmail.com>
2014-05-13 18:57:44 +0000

    * Resource cleanup.

--- FreeCompose/branches/new-approach/FreeCompose/resource.h
+++ FreeCompose/branches/new-approach/FreeCompose/resource.h
@@ -3,20 +3,20 @@
 // Used by FreeCompose.rc
 //
 #define IDP_OLE_INIT_FAILED             100
-#define IDD_ABOUTBOX                    100
 #define IDR_MAINFRAME                   128
 #define IDM_TRAY_MENU                   129
-#define IDD_KEYSEQUENCES                130
-#define IDD_FEATURES                    131
-#define IDD_EDITKEYSEQUENCE             132
-#define IDD_SOUNDS                      133
+#define IDD_ABOUTBOX                    130
+#define IDD_KEYSEQUENCES                131
+#define IDD_FEATURES                    132
+#define IDD_EDITKEYSEQUENCE             133
 #define IDD_EDITKEYSEQUENCE2            134
+#define IDD_SOUNDS                      135
+#define IDADD                           1001
+#define IDEDIT                          1002
+#define IDREMOVE                        1003
 #define IDC_ABOUT_VERSION               2001
 #define IDC_ABOUT_COPYRIGHT             2002
 #define IDC_KEYCOMBOLIST                3001
-#define IDADD                           3002
-#define IDEDIT                          3003
-#define IDREMOVE                        3004
 #define IDC_ENABLEONSTARTUP             4001
 #define IDC_STARTWITHWINDOWS            4002
 #define IDC_COMPOSE_KEY                 4003
@@ -32,6 +32,13 @@
 #define IDC_RESULT_ASHEXCODEPOINT       5103
 #define IDC_RESULT_ASDECCODEPOINT       5104
 #define IDC_EDITRESULT                  5105
+#define IDS_MAINFRAME_ENABLED           6001
+#define IDS_MAINFRAME_DISABLED          6002
+#define IDS_MAINFRAME_MISMATCH_TITLE    6003
+#define IDS_MAINFRAME_MISMATCH_PROMPT   6004
+#define IDS_MAINFRAME_MENU_ENABLE       6005
+#define IDS_MAINFRAME_MENU_DISABLE      6006
+#define IDS_MAINFRAME_CLOSEOPTIONSFIRST 6007
 #define IDS_VK_NAMES_BASE               8000
 #define IDS_VK_LCONTROL                 8000
 #define IDS_VK_RCONTROL                 8001
@@ -48,23 +55,19 @@
 #define IDS_VK_PAUSE                    8012
 #define IDS_VK_TAB                      8013
 #define IDS_VK_BACK                     8014
-#define IDS_ABOUT_VERSION               22001
-#define IDS_EDITKEYSEQUENCE_TITLE_EDIT  22002
-#define IDS_EDITKEYSEQUENCE_TITLE_ADD   22003
-#define IDS_EDITKEYSEQUENCE_CANTBEBLANK 22004
-#define IDS_KEYSEQUENCES_SEQUENCE       22005
-#define IDS_KEYSEQUENCES_RESULT_CODEPOINT 22006
-#define IDS_KEYSEQUENCES_RESULT_CHARACTER 22007
-#define IDS_KEYSEQUENCES_CONFIRM_TITLE  22008
-#define IDS_KEYSEQUENCES_CONFIRM_PROMPT 22009
-#define IDS_MAINFRAME_ENABLED           22010
-#define IDS_MAINFRAME_DISABLED          22011
-#define IDS_MAINFRAME_MISMATCH_TITLE    22012
-#define IDS_MAINFRAME_MISMATCH_PROMPT   22013
-#define IDS_MAINFRAME_MENU_ENABLE       22014
-#define IDS_MAINFRAME_MENU_DISABLE      22015
-#define IDS_OPTIONSPROPSHEET_TITLE      22016
-#define IDS_MAINFRAME_CLOSEOPTIONSFIRST 22017
+#define IDS_ABOUT_VERSION               10001
+#define IDS_OPTIONSPROPSHEET_TITLE      11001
+#define IDS_KEYSEQUENCES_SEQUENCE       12001
+#define IDS_KEYSEQUENCES_RESULT_CODEPOINT 12002
+#define IDS_KEYSEQUENCES_RESULT_CHARACTER 12003
+#define IDS_KEYSEQUENCES_ENABLED        12004
+#define IDS_KEYSEQUENCES_CASEINSENSITIVE 12005
+#define IDS_KEYSEQUENCES_REVERSIBLE     12006
+#define IDS_KEYSEQUENCES_CONFIRM_TITLE  12501
+#define IDS_KEYSEQUENCES_CONFIRM_PROMPT 12502
+#define IDS_EDITKEYSEQUENCE_TITLE_EDIT  13001
+#define IDS_EDITKEYSEQUENCE_TITLE_ADD   13002
+#define IDS_EDITKEYSEQUENCE_CANTBEBLANK 13003
 #define IDS_APP_WINVER_TOO_LOW          23001
 #define IDS_APPSOUND_COMPOSITION_STARTING 24001
 #define IDS_APPSOUND_COMPOSITION_KEYPRESSED 24002
@@ -85,7 +88,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        135
 #define _APS_NEXT_COMMAND_VALUE         32773
-#define _APS_NEXT_CONTROL_VALUE         1006
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_CONTROL_VALUE         30001
+#define _APS_NEXT_SYMED_VALUE           136
 #endif
 #endif
