b20d16ca363a3ff6b76d57994987386d140ff578
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-05-19 16:25:53 +0000

    * Move the bodies of Utf32ToUtf16() (both), Utf16ToUtf32(), and CountCompositeCharacters() (both) to new file Unicode.cpp. * Mark functions FCSHARED_API.

---
+++ FreeCompose/branches/new-approach/FCShared/Unicode.cpp
@@ -0,0 +1,91 @@
+#include "stdafx.h"
+
+#include "FCShared.h"
+#include "Unicode.h"
+
+CString Utf32ToUtf16( UChar32 const* pqz, int const cch ) {
+	UChar* pwzDest = nullptr;
+	int cchDest = 0;
+
+	UErrorCode errorCode = U_ZERO_ERROR;
+	u_strFromUTF32( nullptr, 0, &cchDest, pqz, cch, &errorCode );
+	if ( U_BUFFER_OVERFLOW_ERROR != errorCode ) {
+		debug( L"Utf32ToUtf16/n: u_strFromUTF32 failed, errorCode=%d\n", errorCode );
+		return CString( );
+	}
+
+	int cchDestCapacity = cchDest + 1;
+	pwzDest = new UChar[cchDestCapacity];
+	cchDest = 0;
+
+	errorCode = U_ZERO_ERROR;
+	u_strFromUTF32( pwzDest, cchDestCapacity, &cchDest, pqz, cch, &errorCode );
+	if ( U_ZERO_ERROR != errorCode ) {
+		debug( L"Utf32ToUtf16/n: u_strFromUTF32 failed, errorCode=%d\n", errorCode );
+		delete[] pwzDest;
+		return CString( );
+	}
+
+	CString tmp( pwzDest, cchDest );
+	delete[] pwzDest;
+	return tmp;
+}
+
+CString Utf32ToUtf16( UChar32 const wch ) {
+	UChar wchDest = L'\0';
+	int cchDest = 1;
+
+	UErrorCode errorCode = U_ZERO_ERROR;
+	u_strFromUTF32( &wchDest, 1, &cchDest, &wch, 1, &errorCode );
+	if ( U_STRING_NOT_TERMINATED_WARNING != errorCode ) {
+		debug( L"Utf32ToUtf16/1: u_strFromUTF32 failed, errorCode=%d\n", errorCode );
+		return CString( );
+	} else {
+		return CString( wchDest );
+	}
+}
+
+UChar32* Utf16ToUtf32( UChar const* pwz, int const cch ) {
+	UChar32* pqzDest = nullptr;
+	int cchDest = 0;
+
+	UErrorCode errorCode = U_ZERO_ERROR;
+	u_strToUTF32( nullptr, 0, &cchDest, pwz, cch, &errorCode );
+	if ( U_BUFFER_OVERFLOW_ERROR != errorCode ) {
+		debug( L"Utf16ToUtf32: u_strToUTF32 failed, errorCode=%d\n", errorCode );
+		return nullptr;
+	}
+
+	int cchDestCapacity = cchDest + 1;
+	pqzDest = new UChar32[cchDestCapacity];
+	cchDest = 0;
+
+	errorCode = U_ZERO_ERROR;
+	u_strToUTF32( pqzDest, cchDestCapacity, &cchDest, pwz, cch, &errorCode );
+	if ( U_ZERO_ERROR != errorCode ) {
+		debug( L"Utf16ToUtf32: u_strToUTF32 failed, errorCode=%d\n", errorCode );
+		delete[] pqzDest;
+		return nullptr;
+	}
+
+	return pqzDest;
+}
+
+int CountCompositeCharacters( UChar const* pwz, int const cch ) {
+	icu::UnicodeString unicodeString( icu::UnicodeString( pwz, cch ) );
+	UErrorCode error = U_ZERO_ERROR;
+	icu::BreakIterator* pbi = icu::BreakIterator::createCharacterInstance( icu::Locale::getDefault( ), error );
+	pbi->setText( unicodeString );
+
+	int cComposites = 0;
+	int32_t p = pbi->first( );
+	while ( icu::BreakIterator::DONE != p ) {
+		cComposites++;
+		p = pbi->next( );
+	}
+	return p - 1;
+}
+
+int CountCompositeCharacters( UChar32 const* pqz, int const cch ) {
+	return CountCompositeCharacters( Utf32ToUtf16( pqz, cch ), cch );
+}
--- FreeCompose/branches/new-approach/SharedInclude/Unicode.h
+++ FreeCompose/branches/new-approach/SharedInclude/Unicode.h
@@ -8,88 +8,21 @@
 // Inline functions
 //==============================================================================
 
-template<typename T>
-inline bool const IsSurrogate( T const ch ) {
+inline FCSHARED_API bool const IsSurrogate( UChar32 const ch ) {
 	return ( U_SURROGATE == u_charType( ch ) );
 }
 
-template<typename T>
-inline bool const IsLeadSurrogate( T const ch ) {
+inline FCSHARED_API bool const IsLeadSurrogate( UChar32 const ch ) {
 	return ( UBLOCK_HIGH_SURROGATES == ublock_getCode( ch ) );
 }
 
-template<typename T>
-inline bool const IsTrailSurrogate( T const ch ) {
+inline FCSHARED_API bool const IsTrailSurrogate( UChar32 const ch ) {
 	return ( UBLOCK_LOW_SURROGATES == ublock_getCode( ch ) );
 }
 
+FCSHARED_API CString Utf32ToUtf16( UChar32 const* pqz, int const cch = -1 );
+FCSHARED_API CString Utf32ToUtf16( UChar32 const uch );
+FCSHARED_API UChar32* Utf16ToUtf32( UChar const* pwz, int const cch = -1 );
 
-#ifdef __AFXSTR_H__
-inline CString Utf32ToUtf16( UChar32 const* pqz, int const cch = -1 ) {
-	UChar* pwzDest = nullptr;
-	int cchDest = 0;
-
-	UErrorCode errorCode = U_ZERO_ERROR;
-	u_strFromUTF32( nullptr, 0, &cchDest, pqz, cch, &errorCode );
-	if ( U_BUFFER_OVERFLOW_ERROR != errorCode ) {
-		debug( L"Utf32ToUtf16/n: u_strFromUTF32 failed, errorCode=%d\n", errorCode );
-		return CString( );
-	}
-
-	int cchDestCapacity = cchDest + 1;
-	pwzDest = new UChar[cchDestCapacity];
-	cchDest = 0;
-
-	errorCode = U_ZERO_ERROR;
-	u_strFromUTF32( pwzDest, cchDestCapacity, &cchDest, pqz, cch, &errorCode );
-	if ( U_ZERO_ERROR != errorCode ) {
-		debug( L"Utf32ToUtf16/n: u_strFromUTF32 failed, errorCode=%d\n", errorCode );
-		delete[] pwzDest;
-		return CString( );
-	}
-
-	CString tmp( pwzDest, cchDest );
-	delete[] pwzDest;
-	return tmp;
-}
-
-inline CString Utf32ToUtf16( UChar32 const uch ) {
-	UChar wchDest = L'\0';
-	int cchDest = 1;
-
-	UErrorCode errorCode = U_ZERO_ERROR;
-	u_strFromUTF32( &wchDest, 1, &cchDest, &uch, 1, &errorCode );
-	if ( U_STRING_NOT_TERMINATED_WARNING != errorCode ) {
-		debug( L"Utf32ToUtf16/1: u_strFromUTF32 failed, errorCode=%d\n", errorCode );
-		return CString( );
-	} else {
-		return CString( wchDest );
-	}
-}
-
-inline UChar32* Utf16ToUtf32( UChar const* pwz, int const cch = -1 ) {
-	UChar32* pqzDest = nullptr;
-	int cchDest = 0;
-
-	UErrorCode errorCode = U_ZERO_ERROR;
-	u_strToUTF32( nullptr, 0, &cchDest, pwz, cch, &errorCode );
-	if ( U_BUFFER_OVERFLOW_ERROR != errorCode ) {
-		debug( L"Utf16ToUtf32: u_strToUTF32 failed, errorCode=%d\n", errorCode );
-		return nullptr;
-	}
-
-	int cchDestCapacity = cchDest + 1;
-	pqzDest = new UChar32[cchDestCapacity];
-	cchDest = 0;
-
-	errorCode = U_ZERO_ERROR;
-	u_strToUTF32( pqzDest, cchDestCapacity, &cchDest, pwz, cch, &errorCode );
-	if ( U_ZERO_ERROR != errorCode ) {
-		debug( L"Utf16ToUtf32: u_strToUTF32 failed, errorCode=%d\n", errorCode );
-		delete[] pqzDest;
-		return nullptr;
-	}
-
-	return pqzDest;
-}
-#endif
+FCSHARED_API int CountCompositeCharacters( UChar const* pwz, int const cch = -1 );
+FCSHARED_API int CountCompositeCharacters( UChar32 const* pqz, int const cch = -1 );
