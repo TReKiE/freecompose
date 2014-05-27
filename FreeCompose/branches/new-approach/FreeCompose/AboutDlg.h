edf5a550b40ab6e3b317f77f062d4046a0afb883
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-05-27 15:28:59 +0000

    * Major redesign of About dialog. * Big renumbering of resource IDs, too.

--- FreeCompose/branches/new-approach/FreeCompose/AboutDlg.h
+++ FreeCompose/branches/new-approach/FreeCompose/AboutDlg.h
@@ -5,16 +5,29 @@ class CAboutDlg: public CDialog {
 	DECLARE_MESSAGE_MAP( )
 
 public:
-	enum { IDD = IDD_ABOUTBOX };
+	enum { IDD = IDD_ABOUT };
 
 	CAboutDlg( );
+	virtual ~CAboutDlg( );
 
-protected:
+private:
+	// Member variables
+	CString m_strVersion;
+	CString m_strName;
+
+	CStatic m_staticName;
+	CLinkCtrl m_linkCopyright;
+	CLinkCtrl m_linkWebsite;
+
+	CFont* m_pFont;
+
+	// Methods
+	void _GetVersionInfo( void );
+
 	virtual void DoDataExchange( CDataExchange* pDX );
 	virtual BOOL OnInitDialog( );
 
-	// Implementation
-protected:
-	CString m_strVersion;
-	CString m_strCopyright;
+	afx_msg void OnClickCopyright( NMHDR* pNMHDR, LRESULT* pResult );
+	afx_msg void OnClickWebsite( NMHDR* pNMHDR, LRESULT* pResult );
+
 };
