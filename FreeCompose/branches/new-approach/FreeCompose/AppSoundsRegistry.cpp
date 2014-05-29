e2c4193e9e126a925a7293be727c8438d8350af1
icekarma <icekarma@gmail.com>
2014-05-29 21:36:53 +0000

    * Refactor code for getting current sound scheme out into its own function, CAppSoundsRegistry::_GetCurrentSoundScheme(). * Use MS CRT variable _wpgmptr to obtain full path and file name of executable instead of bothering with GetModuleFileName(). * _RegisterApp(): Change order of registry key creation.

--- FreeCompose/branches/new-approach/FreeCompose/AppSoundsRegistry.cpp
+++ FreeCompose/branches/new-approach/FreeCompose/AppSoundsRegistry.cpp
@@ -34,6 +34,34 @@ int const NumberOfCompositionSounds = _countof( CompositionDisplayNameIds );
 // Implementation
 //
 
+CString CAppSoundsRegistry::_GetCurrentSoundScheme( void ) {
+	CString currentSoundScheme;
+	LSTATUS ls;
+
+	CRegKey schemeskey;
+	ls = schemeskey.Open( HKEY_CURRENT_USER, L"AppEvents\\Schemes", KEY_READ );
+
+	DWORD dwType = 0;
+	ULONG cbValue = 0;
+	ls = schemeskey.QueryValue( nullptr, &dwType, nullptr, &cbValue );
+	if ( ERROR_SUCCESS == ls || ERROR_INSUFFICIENT_BUFFER == ls ) {
+		cbValue += sizeof(wchar_t);
+		wchar_t* pwz = static_cast<wchar_t*>( alloca( cbValue ) );
+		ls = schemeskey.QueryStringValue( nullptr, pwz, &cbValue );
+		if ( ERROR_SUCCESS == ls ) {
+			debug( L"CAppSoundsRegistry::_RegisterApp: Current sound scheme is '%s'\n", pwz );
+			currentSoundScheme = pwz;
+		} else {
+			debug( L"CAppSoundsRegistry::_RegisterApp: Couldn't query value of current sound scheme: error=%lu\n", ls );
+		}
+	} else {
+		debug( L"CAppSoundsRegistry::_GetCurrentSoundScheme: Failed to query size of current sound scheme's name: error=%lu\n", ls );
+	}
+
+	ls = schemeskey.Close( );
+	return currentSoundScheme;
+}
+
 void CAppSoundsRegistry::_RegisterEventLabels( wchar_t const* pwzExeName ) {
 	LSTATUS ls;
 	CRegKey EventLabels;
@@ -62,30 +90,10 @@ void CAppSoundsRegistry::_RegisterApp( wchar_t const* pwzExeName ) {
 	LSTATUS ls;
 	DWORD dwDisposition;
 
-	CString currentSoundScheme;
-	bool silentScheme = false;
-	{
-		CRegKey schemeskey;
-		ls = schemeskey.Open( HKEY_CURRENT_USER, L"AppEvents\\Schemes", KEY_READ );
-
-		DWORD dwType = 0;
-		ULONG cbValue = 0;
-		ls = schemeskey.QueryValue( nullptr, &dwType, nullptr, &cbValue );
-		if ( ERROR_SUCCESS == ls || ERROR_INSUFFICIENT_BUFFER == ls ) {
-			wchar_t* pbuf = tmp.GetBufferSetLength( cbValue + 1 );
-			ls = schemeskey.QueryStringValue( nullptr, pbuf, &cbValue );
-			if ( ERROR_SUCCESS == ls ) {
-				debug( L"CAppSoundsRegistry::_RegisterApp: Current sound scheme is '%s'\n", static_cast<LPCWSTR>( currentSoundScheme ) );
-				if ( 0 == currentSoundScheme.CompareNoCase( L".None" ) ) {
-					debug( L"CAppSoundsRegistry::_RegisterApp: Silent sound scheme in effect!\n" );
-					silentScheme = true;
-				}
-			} else {
-				debug( L"CAppSoundsRegistry::_RegisterApp: Couldn't query value of current sound scheme: error=%lu\n", ls );
-			}
-		} else {
-			debug( L"CAppSoundsRegistry::_RegisterApp: Couldn't determine length of current sound scheme name: error=%lu\n", ls );
-		}
+	CString currentSoundScheme = _GetCurrentSoundScheme( );
+	bool silentScheme = ( 0 == currentSoundScheme.CompareNoCase( L".None" ) );
+	if ( silentScheme ) {
+		debug( L"CAppSoundsRegistry::_RegisterApp: Silent sound scheme is in effect!\n" );
 	}
 
 	tmp.Format( L"@%s,%d", pwzExeName, -AFX_IDS_APP_TITLE );
@@ -105,15 +113,15 @@ void CAppSoundsRegistry::_RegisterApp( wchar_t const* pwzExeName ) {
 			ls = dotdefaultkey.SetStringValue( nullptr, CompositionDefaultSoundSchemeFileName[n] );
 			ls = dotdefaultkey.Close( );
 
-			CRegKey dotcurrentkey;
-			ls = dotcurrentkey.Create( soundkey, L".Current" );
-			ls = dotcurrentkey.SetStringValue( nullptr, silentScheme ? L"" : CompositionDefaultSoundSchemeFileName[n] );
-			ls = dotcurrentkey.Close( );
-
 			CRegKey dotnonekey;
 			ls = dotnonekey.Create( soundkey, L".None" );
 			ls = dotnonekey.SetStringValue( nullptr, L"" );
 			ls = dotnonekey.Close( );
+
+			CRegKey dotcurrentkey;
+			ls = dotcurrentkey.Create( soundkey, L".Current" );
+			ls = dotcurrentkey.SetStringValue( nullptr, silentScheme ? L"" : CompositionDefaultSoundSchemeFileName[n] );
+			ls = dotcurrentkey.Close( );
 
 			ls = soundkey.Close( );
 		}
@@ -155,11 +163,8 @@ void CAppSoundsRegistry::_UnregisterApp( void ) {
 //
 
 void CAppSoundsRegistry::RegisterFcAppSounds( void ) {
-	wchar_t wzExeName[1024];
-	GetModuleFileName( AfxGetInstanceHandle( ), wzExeName, 1024 );
-
-	_RegisterEventLabels( wzExeName );
-	_RegisterApp( wzExeName );
+	_RegisterEventLabels( _wpgmptr );
+	_RegisterApp( _wpgmptr );
 }
 
 void CAppSoundsRegistry::UnregisterFcAppSounds( void ) {
