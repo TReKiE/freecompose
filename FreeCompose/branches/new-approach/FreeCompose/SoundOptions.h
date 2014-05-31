0eef0823c827642e4ed8662f5983ee016cff0607
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-05-31 18:17:18 +0000

    * First rough draft.

--- FreeCompose/branches/new-approach/FreeCompose/SoundOptions.h
+++ FreeCompose/branches/new-approach/FreeCompose/SoundOptions.h
@@ -1,2 +1,67 @@
 #pragma once
 
+class SoundScheme;
+class SoundEvent;
+
+
+class SoundOptions {
+
+public:
+	std::vector<SoundScheme> Schemes;
+
+};
+
+
+class SoundScheme {
+
+public:
+	std::map<CString, SoundEvent> Events;
+
+};
+
+
+class SoundEvent {
+
+public:
+	inline SoundEvent( ) { }
+	inline virtual ~SoundEvent( ) { }
+
+	virtual bool PlaySound( void ) = 0;
+
+};
+
+class NoSoundEvent: public SoundEvent {
+
+public:
+	inline NoSoundEvent( ) { }
+	inline virtual ~NoSoundEvent( ) { }
+
+	inline virtual bool PlaySound( void ) {
+		return false;
+	}
+
+};
+
+class ApplicationSoundEvent: public SoundEvent {
+
+public:
+	inline ApplicationSoundEvent( ) { }
+	inline virtual ~ApplicationSoundEvent( ) { }
+
+	inline virtual bool PlaySound( void ) {
+		return false;
+	}
+
+};
+
+class ToneSoundEvent: public SoundEvent {
+
+public:
+	inline ToneSoundEvent( ) { }
+	inline virtual ~ToneSoundEvent( ) { }
+
+	inline virtual bool PlaySound( void ) {
+		return false;
+	}
+
+};
