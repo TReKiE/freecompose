48b28cf9fb02becfc2ea577208b194ef7863b28f
icekarma <icekarma@gmail.com>
2014-06-02 14:54:47 +0000

    * Add new classes XmlOptionsReader, XmlOptionsWriter.

---
+++ FreeCompose/branches/new-approach/FreeCompose/XmlOptionsReader.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+
+#include "XmlOptionsReader.h"
+
+XmlOptionsReader::XmlOptionsReader( )
+{
+	
+}
+
+XmlOptionsReader::~XmlOptionsReader( )
+{
+	
+}
---
+++ FreeCompose/branches/new-approach/FreeCompose/XmlOptionsWriter.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+
+#include "XmlOptionsWriter.h"
+
+XmlOptionsWriter::XmlOptionsWriter( )
+{
+	
+}
+
+XmlOptionsWriter::~XmlOptionsWriter( )
+{
+	
+}
