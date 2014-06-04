40e8201b9dec57c7238a0865fa0f95817aa19ae4
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-06-04 00:03:06 +0000

    * Yay, more renaming and moving around. * Rearrange FreeCompose.h, add section comments. * Renames:   * CompositionDefaultSoundSchemeFileName => ApplicationSoundDefaultSoundEventFileName   * CompositionDisplayNameIds => ApplicationSoundDisplayNameIds (also now a global constant)   * CompositionSoundNames => ApplicationSoundNames   * NumberOfCompositionSounds => NumberOfApplicationSounds (also now a global constant)   * CAppSoundsRegistry => CFcAppSoundsRegistry     * Member RegisterFcAppSounds => Register     * Member UnregisterFcAppSounds => Unregister * New enum ApplicationSound, for FreeCompose app sounds. * Add string table entries for new sounds: IDS_APPSOUND_APPLICATION{,DE}ACTIVATED. * CSounds:   * Move empty ctor, dtor into header file and mark inline.   * Mark rule-of-five members all "= delete": none of them should exist, and trying to use them is an error.   * Add member m_Schemes to hold a reference to m_Options.Sounds.Schemes, to save two indirections.

--- FreeCompose/branches/new-approach/FreeCompose/FreeCompose.h
+++ FreeCompose/branches/new-approach/FreeCompose/FreeCompose.h
@@ -1,5 +1,31 @@
 #pragma once
 
+//==============================================================================
+// Types
+//==============================================================================
+
+enum class ApplicationSound: int {
+	none = -1,
+	Activated = static_cast<int>( CompositionSound::max ),
+	Deactivated,
+	max
+};
+
+//==============================================================================
+// Constants
+//==============================================================================
+
+extern UINT const APP_ACTIVATE;
+extern UINT const APP_RECONFIGURE;
+extern UINT const APP_NOTIFYICON;
+
+extern UINT const FCM_KEY;
+extern UINT const FCM_COMPOSITION_SOUND;
+
+//==============================================================================
+// Classes
+//==============================================================================
+
 class CFreeComposeApp: public CWinApp {
 	DECLARE_DYNAMIC( CFreeComposeApp )
 	DECLARE_MESSAGE_MAP( )
@@ -14,13 +40,11 @@ protected:
 	HANDLE m_hInstanceMutex;
 
 	bool IsAlreadyRunning( );
+
 };
 
+//==============================================================================
+// Global variables
+//==============================================================================
+
 extern CFreeComposeApp theApp;
-
-extern UINT const APP_ACTIVATE;
-extern UINT const APP_RECONFIGURE;
-extern UINT const APP_NOTIFYICON;
-
-extern UINT const FCM_KEY;
-extern UINT const FCM_COMPOSITION_SOUND;
--- FreeCompose/branches/new-approach/FreeCompose/Sounds.h
+++ FreeCompose/branches/new-approach/FreeCompose/Sounds.h
@@ -7,8 +7,29 @@ class CSounds: public CPropertyPage {
 	DECLARE_MESSAGE_MAP( )
 
 public:
-	CSounds( COptionsData& Options );
-	virtual ~CSounds( );
+	CSounds( ) = delete;
+	CSounds( CSounds const& ) = delete;
+	CSounds( CSounds&& ) = delete;
+	CSounds& operator=( CSounds const& ) = delete;
+	CSounds& operator=( CSounds&& ) = delete;
+
+	inline CSounds( COptionsData& Options ):
+		CPropertyPage  ( IDT_SOUNDS ),
+		m_Options      ( Options ),
+		m_Schemes      ( Options.Sounds.Schemes ),
+		m_nSchemeIndex ( 0 ),
+		m_nEventIndex  ( 0 ),
+		m_nRadioIndex  ( 0 ),
+		m_nFrequency   ( 0 ),
+		m_nDuration    ( 0 )
+	{
+
+	}
+
+	inline ~CSounds( )
+	{
+
+	}
 
 private:
 	COptionsData& m_Options;
@@ -30,6 +51,8 @@ private:
 	CString m_strFileName;
 	int m_nFrequency;
 	int m_nDuration;
+
+	SoundSchemeVector& m_Schemes;
 
 	virtual void DoDataExchange( CDataExchange* pDX );
 	virtual BOOL OnInitDialog( );
