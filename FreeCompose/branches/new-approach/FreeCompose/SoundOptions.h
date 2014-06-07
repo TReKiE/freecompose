50b05e5bc02cf48f9d1b015b473adb9e99387e67
icekarma@gmail.com <icekarma@gmail.com@bea30d8e-86d1-9815-9a41-0b703c73c987>
2014-06-07 21:20:41 +0000

    * Enum SoundOptions: Apply the indexable trait. * Add noisydebug messages (and whitespace) to constructors and destructors. * Add move constructor and operator= to everything. * Change move and copy constructors' invocations of operator=(), to make it actually call the right one. (This isn't a theoretical concern: I verified that this was causing issues, in the debugger.) * SoundOptions::operator=(): I don't apparently understand how to use std::copy() (it's throwing a "your doing it rong"), so use a for loop instead. * SoundOptions::GetSoundScheme(): Adjust const-ness in a few places.

--- FreeCompose/branches/new-approach/FreeCompose/SoundOptions.h
+++ FreeCompose/branches/new-approach/FreeCompose/SoundOptions.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifndef _NOISY_DEBUG
+#	pragma push_macro( "noisydebug" )
+#	undef  noisydebug
+#	define noisydebug debug
+#endif
+
 //
 // Forward declarations
 //
@@ -11,7 +17,7 @@ class SoundEvent;
 // Types
 //
 
-enum class SoundEventType: int {
+DECLARE_INDEXABLE_TYPED_ENUM( SoundEventType, int ) {
 	none             = -1,
 	NoSound          =  0,
 	ApplicationSound,
@@ -28,21 +34,45 @@ using SoundSchemeVector = std::vector<SoundScheme>;
 class SoundOptions {
 
 public:
-	inline SoundOptions( ) { }
-	inline SoundOptions( SoundOptions const& rhs ) { *this = rhs; }
-	inline ~SoundOptions( ) { }
+	inline SoundOptions( ) {
+		noisydebug( L"0x%p SoundOptions::`ctor/1\n", this );
+	}
+
+	inline SoundOptions( SoundOptions const& rhs ) {
+		noisydebug( L"0x%p SoundOptions::`ctor/2\n", this );
+		operator=( static_cast<SoundOptions const&>( rhs ) );
+	}
+
+	inline SoundOptions( SoundOptions&& rhs ) {
+		noisydebug( L"0x%p SoundOptions::`ctor/3\n", this );
+		operator=( static_cast<SoundOptions&&>( rhs ) );
+	}
+
+	inline ~SoundOptions( ) {
+		noisydebug( L"0x%p SoundOptions::`dtor\n", this );
+	}
 
 	inline SoundOptions& operator=( SoundOptions const& rhs ) {
-		std::copy( rhs.Schemes.begin( ), rhs.Schemes.end( ), Schemes.begin( ) );
+		Schemes.clear( );
+		for ( auto& scheme : rhs.Schemes ) {
+			Schemes.push_back( scheme );
+		}
+		return *this;
+	}
+
+	inline SoundOptions& operator=( SoundOptions&& rhs ) {
+		*this = const_cast<SoundOptions const&>( rhs );
+		rhs.Schemes.clear( );
 		return *this;
 	}
 
 	inline bool operator==( SoundOptions const& rhs ) const;
+
 	inline bool operator!=( SoundOptions const& rhs ) const {
 		return !( *this == rhs );
 	}
 
-	inline SoundScheme* GetSoundScheme( CString const& name );
+	inline SoundScheme* GetSoundScheme( CString const& name ) const;
 
 	SoundSchemeVector Schemes;
 
@@ -58,9 +88,24 @@ using SoundEventMapPair = std::pair<CString, SoundEvent*>;
 class SoundScheme {
 
 public:
-	inline SoundScheme( ) { }
-	inline SoundScheme( SoundScheme const& rhs ) { *this = rhs; }
-	inline SoundScheme( CString const& id, CString const& name ): ID( id ), Name( name ) { }
+	inline SoundScheme( ) {
+		noisydebug( L"0x%p SoundScheme::`ctor/1\n", this );
+	}
+
+	inline SoundScheme( SoundScheme const& rhs ) {
+		noisydebug( L"0x%p SoundScheme::`ctor/2\n", this );
+		operator=( static_cast<SoundScheme const&>(rhs) );
+	}
+
+	inline SoundScheme( SoundScheme&& rhs ) {
+		noisydebug( L"0x%p SoundScheme::`ctor/3\n", this );
+		operator=( static_cast<SoundScheme&&>(rhs) );
+	}
+
+	inline SoundScheme( CString const& id, CString const& name ): ID( id ), Name( name ) {
+		noisydebug( L"0x%p SoundScheme::`ctor/4\n", this );
+	}
+
 	inline ~SoundScheme( );
 
 	inline SoundScheme& operator=( SoundScheme const& rhs ) {
@@ -69,6 +114,14 @@ public:
 		for ( auto& pair : rhs.Events ) {
 			Events.insert( SoundEventMapPair( pair.first, pair.second ) );
 		}
+		return *this;
+	}
+
+	inline SoundScheme& operator=( SoundScheme&& rhs ) {
+		*this = const_cast<SoundScheme const&>( rhs );
+		rhs.ID.Empty( );
+		rhs.Name.Empty( );
+		rhs.Events.clear( );
 		return *this;
 	}
 
@@ -91,10 +144,10 @@ public:
 };
 
 // The following method is defined here because class SoundScheme is incomplete until this point.
-inline SoundScheme* SoundOptions::GetSoundScheme( CString const& name ) {
+inline SoundScheme* SoundOptions::GetSoundScheme( CString const& name ) const {
 	for ( auto& s : Schemes ) {
 		if ( 0 == s.Name.CompareNoCase( name ) ) {
-			return &s;
+			return const_cast<SoundScheme*>( &s );
 		}
 	}
 	return nullptr;
@@ -132,13 +185,36 @@ inline bool SoundOptions::operator==( SoundOptions const& rhs ) const {
 class SoundEvent {
 
 public:
-	inline SoundEvent( ) { }
-	inline SoundEvent( CString const& name ): Name( name ) { }
-	inline SoundEvent( SoundEvent const& rhs ) { *this = rhs; }
-	inline virtual ~SoundEvent( ) { }
+	inline SoundEvent( ) {
+		noisydebug( L"0x%p SoundEvent::`ctor/1\n", this );
+	}
+
+	inline SoundEvent( SoundEvent const& rhs ) {
+		noisydebug( L"0x%p SoundEvent::`ctor/2\n", this );
+		operator=( static_cast<SoundEvent const&>( rhs ) );
+	}
+
+	inline SoundEvent( SoundEvent&& rhs ) {
+		noisydebug( L"0x%p SoundEvent::`ctor/3\n", this );
+		operator=( static_cast<SoundEvent&&>( rhs ) );
+	}
+
+	inline SoundEvent( CString const& name ): Name( name ) {
+		noisydebug( L"0x%p SoundEvent::`ctor/4\n", this );
+	}
+
+	inline virtual ~SoundEvent( ) {
+		noisydebug( L"0x%p SoundEvent::`dtor\n", this );
+	}
 
 	inline virtual SoundEvent& operator=( SoundEvent const& rhs ) {
 		Name = rhs.Name;
+		return *this;
+	}
+
+	inline virtual SoundEvent& operator=( SoundEvent&& rhs ) {
+		*this = const_cast<SoundEvent const&>( rhs );
+		rhs.Name.Empty( );
 		return *this;
 	}
 
@@ -164,6 +240,7 @@ public:
 
 // The following method is defined here because class SoundEvent is incomplete until this point.
 inline SoundScheme::~SoundScheme( ) {
+	noisydebug( L"0x%p SoundScheme::`dtor\n", this );
 	for ( auto p : Events ) {
 		delete p.second;
 	}
@@ -177,10 +254,27 @@ inline SoundScheme::~SoundScheme( ) {
 class NoSoundEvent: public SoundEvent {
 
 public:
-	inline NoSoundEvent( ): SoundEvent( ) { }
-	inline NoSoundEvent( NoSoundEvent const& rhs ) { *this = rhs; }
-	inline NoSoundEvent( CString const& name ): SoundEvent( name ) { }
-	inline virtual ~NoSoundEvent( ) { }
+	inline NoSoundEvent( ): SoundEvent( ) {
+		noisydebug( L"0x%p NoSoundEvent::`ctor/1\n", this );
+	}
+
+	inline NoSoundEvent( NoSoundEvent const& rhs ) {
+		noisydebug( L"0x%p NoSoundEvent::`ctor/2\n", this );
+		operator=( static_cast<NoSoundEvent const&>( rhs ) );
+	}
+
+	inline NoSoundEvent( NoSoundEvent&& rhs ) {
+		noisydebug( L"0x%p NoSoundEvent::`ctor/3\n", this );
+		operator=( static_cast<NoSoundEvent&&>( rhs ) );
+	}
+
+	inline NoSoundEvent( CString const& name ): SoundEvent( name ) {
+		noisydebug( L"0x%p NoSoundEvent::`ctor/4\n", this );
+	}
+
+	inline virtual ~NoSoundEvent( ) {
+		noisydebug( L"0x%p NoSoundEvent::`dtor\n", this );
+	}
 
 	inline virtual SoundEventType GetType( void ) const {
 		return SoundEventType::NoSound;
@@ -199,10 +293,27 @@ public:
 class ApplicationSoundEvent: public SoundEvent {
 
 public:
-	inline ApplicationSoundEvent( ): SoundEvent( ) { }
-	inline ApplicationSoundEvent( ApplicationSoundEvent const& rhs ) { *this = rhs; }
-	inline ApplicationSoundEvent( CString const& name ): SoundEvent( name ) { }
-	inline virtual ~ApplicationSoundEvent( ) { }
+	inline ApplicationSoundEvent( ): SoundEvent( ) {
+		noisydebug( L"0x%p ApplicationSoundEvent::`ctor/1\n", this );
+	}
+
+	inline ApplicationSoundEvent( ApplicationSoundEvent const& rhs ) {
+		noisydebug( L"0x%p ApplicationSoundEvent::`ctor/2\n", this );
+		operator=( static_cast<ApplicationSoundEvent const&>( rhs ) );
+	}
+
+	inline ApplicationSoundEvent( ApplicationSoundEvent&& rhs ) {
+		noisydebug( L"0x%p ApplicationSoundEvent::`ctor/3\n", this );
+		operator=( static_cast<ApplicationSoundEvent&&>( rhs ) );
+	}
+
+	inline ApplicationSoundEvent( CString const& name ): SoundEvent( name ) {
+		noisydebug( L"0x%p ApplicationSoundEvent::`ctor/4\n", this );
+	}
+
+	inline virtual ~ApplicationSoundEvent( ) {
+		noisydebug( L"0x%p ApplicationSoundEvent::`dtor\n", this );
+	}
 
 	inline virtual SoundEventType GetType( void ) const {
 		return SoundEventType::ApplicationSound;
@@ -221,15 +332,39 @@ public:
 class ToneSoundEvent: public SoundEvent {
 
 public:
-	inline ToneSoundEvent( ): SoundEvent( ), Frequency( 0 ), Duration( 0 ) { }
-	inline ToneSoundEvent( ToneSoundEvent const& rhs ) { *this = rhs; }
-	inline ToneSoundEvent( CString const& name, unsigned const frequency, unsigned const duration ): SoundEvent( name ), Frequency( frequency ), Duration( duration ) { }
-	inline virtual ~ToneSoundEvent( ) { }
+	inline ToneSoundEvent( ): SoundEvent( ), Frequency( 0 ), Duration( 0 ) {
+		noisydebug( L"0x%p ToneSoundEvent::`ctor/1\n", this );
+	}
+
+	inline ToneSoundEvent( ToneSoundEvent const& rhs ) {
+		noisydebug( L"0x%p ToneSoundEvent::`ctor/2\n", this );
+		operator=( static_cast<ToneSoundEvent const&>( rhs ) );
+	}
+
+	inline ToneSoundEvent( ToneSoundEvent&& rhs ) {
+		noisydebug( L"0x%p ToneSoundEvent::`ctor/3\n", this );
+		operator=( static_cast<ToneSoundEvent&&>( rhs ) );
+	}
+
+	inline ToneSoundEvent( CString const& name, unsigned const frequency, unsigned const duration ): SoundEvent( name ), Frequency( frequency ), Duration( duration ) {
+		noisydebug( L"0x%p ToneSoundEvent::`ctor/4\n", this );
+	}
+
+	inline virtual ~ToneSoundEvent( ) {
+		noisydebug( L"0x%p ToneSoundEvent::`dtor\n", this );
+	}
 
 	inline virtual ToneSoundEvent& operator=( ToneSoundEvent const& rhs ) {
 		SoundEvent::operator=( rhs );
 		Frequency = rhs.Frequency;
 		Duration = rhs.Duration;
+		return *this;
+	}
+
+	inline virtual ToneSoundEvent& operator=( ToneSoundEvent&& rhs ) {
+		*this = const_cast<ToneSoundEvent const&>( rhs );
+		rhs.Frequency = 0;
+		rhs.Duration = 0;
 		return *this;
 	}
 
@@ -256,3 +391,5 @@ public:
 	unsigned Duration;
 
 };
+
+#pragma pop_macro( "noisydebug" )
