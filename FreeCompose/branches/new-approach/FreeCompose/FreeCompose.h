b23c73c49796d718bfc2734889824672b245e721
icekarma <icekarma@gmail.com>
2014-06-10 16:24:05 +0000

    * Move enum class ConfigurationSource, global g_ConfigurationSource to FreeCompose.h. * Add new registered window message APP_SHOWOPTIONS, sent by CMainFrame::_Initialize() if g_ShowOptionsDialogImmediately is true.

--- FreeCompose/branches/new-approach/FreeCompose/FreeCompose.h
+++ FreeCompose/branches/new-approach/FreeCompose/FreeCompose.h
@@ -11,6 +11,12 @@ DECLARE_INDEXABLE_ENUM( ApplicationSound ) {
 	max
 };
 
+enum class ConfigurationSource {
+	User,
+	Registry,
+	Default,
+};
+
 //==============================================================================
 // Constants
 //==============================================================================
@@ -18,6 +24,7 @@ DECLARE_INDEXABLE_ENUM( ApplicationSound ) {
 extern UINT const APP_ACTIVATE;
 extern UINT const APP_RECONFIGURE;
 extern UINT const APP_NOTIFYICON;
+extern UINT const APP_SHOWOPTIONS;
 
 extern UINT const FCM_KEY;
 extern UINT const FCM_COMPOSITION_SOUND;
@@ -48,3 +55,6 @@ protected:
 //==============================================================================
 
 extern CFreeComposeApp theApp;
+extern ConfigurationSource g_ConfigurationSource;
+extern bool g_ShowOptionsDialogImmediately;
+
